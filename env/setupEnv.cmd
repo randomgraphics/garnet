5653db3badc5b0f945d6ee960f1f25c9ca6d3085
chenli <chenlirandom@users.noreply.github.com>
2005-08-02 09:50:36 +0000

    fix path bug

--- env/setupEnv.cmd
+++ env/setupEnv.cmd
@@ -7,12 +7,12 @@ popd
 REM ==========
 REM setup path
 REM ==========
-if "AMD64" == "PROCESSOR_ARCHITECTURE" (
-    set PATH=%GARNET_ROOT%\env\bin\x64;%GARNET_ROOT%\env\bin\win32;%PATH%
+if "AMD64" == "%PROCESSOR_ARCHITECTURE%" (
+    set PATH=%GARNET_ROOT%\env\bin\x64;%GARNET_ROOT%\env\bin\win32;%GARNET_ROOT%\env\bin\common;%PATH%
 ) else (
-    set PATH=%GARNET_ROOT%\env\bin\win32;%PATH%
+    set PATH=%GARNET_ROOT%\env\bin\win32;%GARNET_ROOT%\env\bin\common;%PATH%
 )
-set PATH=%GARNET_ROOT%\env\bin\common;%PATH%
+
 
 REM ===========
 REM setup scons
