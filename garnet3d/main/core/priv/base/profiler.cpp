a951776ef17697d0788ed38d0c5f6a6b810863ce
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-08-04 12:25:43 +0000

    1. refine clock implementation: change system frequency data member to static 2. new class: light weight profiler.

---
+++ garnet3d/main/core/priv/base/profiler.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "garnet/base/profiler.h"
+
+//
+//
+// ----------------------------------------------------------------------------
+void GN::ProfilerManager::toString( GN::StrA & rval ) const
+{
+    if( mTimers.empty() ) { rval = ""; return; }
+
+    rval =
+        "\n"
+        "=======================================\n"
+        "            profile result\n"
+        "---------------------------------------\n";
+    std::map<const char *,TimerDesc>::const_iterator i, e = mTimers.end();
+    for( i = mTimers.begin(); i != e; ++i )
+    {
+        const TimerDesc & t = i->second;
+        rval += GN::formatStr(
+            "    %s : count(%.0f), sum(%.0f), ave(%.0f), min(%.0f), max(%.0f)\n",
+            i->first,
+            t.count,
+            t.timesum,
+            t.timesum / t.count,
+            t.timemin,
+            t.timemax );
+    }
+    rval +=
+        "=======================================\n"
+        "\n";
+}
+
---
+++ garnet3d/main/core/priv/inc/garnet/base/profiler.h
@@ -0,0 +1,162 @@
+#ifndef __GN_BASE_PROFILER_H__
+#define __GN_BASE_PROFILER_H__
+// *****************************************************************************
+//! \file    profiler.h
+//! \brief   light-weight profiler
+//! \author  chenlee (2005.8.4)
+// *****************************************************************************
+
+#include <limits>
+#include <map>
+
+namespace GN
+{
+    //!
+    //! Profiler Manager
+    //!
+    class ProfilerManager
+    {
+        // ********************************
+        //! \name  ctor/dtor
+        // ********************************
+
+        //@{
+    protected:
+        ProfilerManager();
+        virtual ~ProfilerManager();
+        //@}
+
+        // ********************************
+        //   public functions
+        // ********************************
+    public:
+
+        //!
+        //! reset profiler, clear all timers
+        //!
+        void reset() { mTimers.clear(); mClock.reset(); }
+
+        //!
+        //! print profile result to string
+        //!
+        void toString( StrA & ) const;
+
+        //!
+        //! print profile result to string
+        //!
+        StrA toString() { StrA str; toString(str); return str; }
+
+        //!
+        //! start a profile timer
+        //!
+        void startTimer( const char * name )
+        {
+            GN_GUARD_SLOW;
+            mTimers[name].start( mClock );
+            GN_UNGUARD_SLOW;
+        }
+
+        //!
+        //! stop a profile timer
+        //!
+        void stopTimer( const char * name )
+        {
+            GN_GUARD_SLOW;
+            mTimers[name].stop( mClock );
+            GN_UNGUARD_SLOW;
+        }
+
+        // ********************************
+        //   private variables
+        // ********************************
+    private:
+
+        //!
+        //! private timer structure
+        //!
+        struct TimerDesc
+        {
+            double count, timesum, timemin, timemax, timestart;
+
+            TimerDesc()
+                : count(0)
+                , timesum(0)
+                , timemin( std::numeric_limits<double>::max() )
+                , timemax( 0 )
+                , timestart(0)
+            {}
+
+            void start( Clock & c )
+            {
+                GN_ASSERT( 0 == timestart );
+                timestart = c.getTimeD();
+            }
+
+            void stop( Clock & c )
+            {
+                GN_ASSERT( 0 != timestart );
+                double t = c.getTimeD() - timestart;
+                if( t < timemin ) timemin = t;
+                if( t > timemax ) timemax = t;
+                timesum += t;
+                timestart = 0;
+                ++count;
+            }
+        };
+
+        Clock                            mClock;
+        std::map<const char *,TimerDesc> mTimers;
+
+        // ********************************
+        //   private functions
+        // ********************************
+    private:
+    };
+
+    //!
+    //! scope timer
+    //!
+    class ScopeTimer
+    {
+        ProfilerManager * mMgr;
+        const char * mName;
+
+    public :
+
+        //!
+        //! start the timer
+        //!
+        ScopeTimer( ProfilerManager * mgr, const char * name )
+            : mMgr(mgr), mName(name)
+        {
+            GN_ASSERT( mgr && name );
+            mgr->startTimer(name);
+        }
+
+        //!
+        //! end the timer
+        //!
+        ~ScopeTimer()
+        {
+            stop();
+        }
+
+        //!
+        //! end the timer manally
+        //!
+        void stop()
+        {
+            if( mName )
+            {
+                GN_ASSERT( mMgr );
+                mMgr->stopTimer(mName);
+                mName = 0;
+            }
+        }
+    };
+}
+
+// *****************************************************************************
+//                           End of profiler.h
+// *****************************************************************************
+#endif // __GN_BASE_PROFILER_H__
