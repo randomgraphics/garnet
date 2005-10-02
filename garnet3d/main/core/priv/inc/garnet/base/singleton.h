dcc7dca2463fe5f5cd98a8b6ab89c7f86373aebf
chen <chen@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-10-02 01:59:49 +0000

    new template: LocalSingleton<T> for inside-DLL usage.

--- garnet3d/main/core/priv/inc/garnet/base/singleton.h
+++ garnet3d/main/core/priv/inc/garnet/base/singleton.h
@@ -9,8 +9,7 @@
 namespace GN
 {
     //!
-    //! 单件类. This class can not be used accross DLL boundary. If you want to
-    //! implement a cross-DLL singleton class, please use singleton macros.
+    //! 单件类.
     //!
     template<typename T>
     class Singleton
@@ -45,12 +44,51 @@ namespace GN
         //!
         static T * getInstancePtr() { return msInstancePtr; }
     };
-}
 
-//!
-//! 实现单件类中的静态变量
-//!
-#define GN_IMPLEMENT_SINGLETON(T) template<> T * ::GN::Singleton< T >::msInstancePtr = 0;
+    //!
+    //! 实现单件类中的静态变量
+    //!
+    #define GN_IMPLEMENT_SINGLETON(T) template<> T * ::GN::Singleton< T >::msInstancePtr = 0;
+
+    //!
+    //! 用于局部使用的单件类，无法跨DLL使用。
+    //!
+    template<typename T>
+    class LocalSingleton
+    {
+        static T * msInstancePtr; //!< 指向singleton的实例
+
+    public:
+
+        //!
+        //! Constructor
+        //!
+        LocalSingleton()
+        {
+            GN_ASSERT( 0 == msInstancePtr );
+            // This is code 64-bit compatible?
+            size_t offset = (size_t)(T*)1 - (size_t)(Singleton<T>*)(T*)1;
+            msInstancePtr = (T*)((size_t)this+offset);
+        }
+
+        //!
+        //! Destructor
+        //!
+        virtual ~LocalSingleton() { GN_ASSERT(msInstancePtr); msInstancePtr = 0; }
+
+        //!
+        //! Get the instance
+        //!
+        static T & getInstance() { GN_ASSERT(msInstancePtr); return *msInstancePtr; }
+
+        //!
+        //! Get the instance pointer (might be NULL)
+        //!
+        static T * getInstancePtr() { return msInstancePtr; }
+    };
+
+    template<typename T> T * LocalSingleton<T>::msInstancePtr = 0;
+}
 
 // *****************************************************************************
 //                           End of singleton.h
