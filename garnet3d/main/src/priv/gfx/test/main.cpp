01957021d26e1d9a104277235e165265414d2242
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-10-09 13:00:53 +0000

    new GFX test module.

---
+++ garnet3d/main/src/priv/gfx/test/main.cpp
@@ -0,0 +1,140 @@
+#include "pch.h"
+
+//!
+//! GFX module test application
+//!
+class GfxTest
+{
+    GN::SharedLib mLib;
+    GN::gfx::CreateRendererFunc mCreator;
+    GN::AutoObjPtr<GN::gfx::Renderer> mRenderer;
+
+    bool mDone;
+
+public:
+
+    //!
+    //! Default constructor
+    //!
+    GfxTest() {}
+
+    //!
+    //! Destructor
+    //!
+    ~GfxTest() { quit(); }
+
+    //!
+    //! Initialize test application
+    //!
+    bool init( const char * api )
+    {
+        GN::StrA libName = GN::StrA("GNgfx") + api;
+        if( !mLib.load( libName.cstr() ) ) return false;
+        mCreator = (GN::gfx::CreateRendererFunc)mLib.getSymbol( "GNgfxCreateRenderer" );
+        if( !mCreator ) return false;
+
+        GN::gfx::DeviceSettings ds;
+        mRenderer.reset( mCreator(ds) );
+        if( !mRenderer ) return false;
+
+        mDone = false;
+
+        // success
+        return true;
+    }
+
+    //!
+    //! Quit test application
+    //!
+    void quit()
+    {
+        mRenderer.reset();
+        mCreator = 0;
+        mLib.free();
+    }
+
+    //!
+    //! Run test application
+    //!
+    int run()
+    {
+        if( !mRenderer )
+        {
+            GN_ERROR( "GfxTest is not initialized!" );
+            return -1;
+        }
+
+        while(!mDone)
+        {
+            update();
+            if( mRenderer->drawBegin() )
+            {
+                render();
+                mRenderer->drawEnd();
+            }
+        }
+
+        return 0; 
+    }
+
+    //!
+    //! Frame update
+    //!
+    void update()
+    {
+#if GN_WINNT
+    mDone = !!(0x8000 & ::GetKeyState( VK_ESCAPE ));
+#endif
+    }
+
+    //!
+    //! Frame render
+    //!
+    void render()
+    {
+        mRenderer->clearScreen( GN::Vector4f(0,0,1,1) ); // clear to pure blue.
+    }
+};
+
+#if GN_WINNT
+#define DEFAULT_MODULE "D3D"
+#else
+#define DEFAULT_MODULE "OGL"
+#endif
+
+//!
+//! Print usage
+//!
+void usage( const char * appName )
+{
+    printf(
+        "GFX module test application.\n"
+        "\n"
+        "Usage: %s [D3D|OGL]\n"
+        "\n"
+        "Note: default module is %s\n",
+        appName,
+        DEFAULT_MODULE );
+}
+
+//!
+//! Main entry point
+//!
+int main( int argc, const char * argv[] )
+{
+    const char * module;
+    
+    if( argc < 2 )
+    {
+        usage( argv[0] );
+        module = DEFAULT_MODULE;
+    }
+    else
+    {
+        module = argv[1];
+    }
+
+    GfxTest app;
+    if( !app.init( module ) ) return -1;
+    return app.run();
+}
---
+++ garnet3d/main/src/priv/gfx/test/pch.h
@@ -0,0 +1,53 @@
+#ifndef __GN_GFX_PCH_H__
+#define __GN_GFX_PCH_H__
+// *****************************************************************************
+// \file    pch.h
+// \brief   PCH header
+// \author  chenlee (2005.10.9)
+// *****************************************************************************
+
+#include "garnet/GNbase.h"
+#include "garnet/GNcore.h"
+#include "garnet/GNgfx.h"
+
+#if GN_WINNT
+#define NOMINMAX
+#include <windows.h>
+#endif
+
+#include <exception>
+#include <malloc.h>
+#include <stdlib.h>
+
+#include "garnet/base/pragma.h"
+
+//!
+//! gfx module fatal error
+//!
+#define GNGFX_FATAL GN_FATAL
+
+//!
+//! gfx module error log macro
+//!
+#define GNGFX_ERROR GN_ERROR
+
+//!
+//! gfx module warning log macro
+//!
+#define GNGFX_WARN GN_WARN
+
+//!
+//! gfx module informational log macro
+//!
+#define GNGFX_INFO GN_INFO
+
+//!
+//! gfx module tracing macro
+//!
+#define GNGFX_TRACE GN_TRACE
+
+
+// *****************************************************************************
+//                           End of pch.h
+// *****************************************************************************
+#endif // __GN_GFX_PCH_H__
