c3c957dbc3cb88d7331bea5d3b57fb004f36ac0f
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-10-10 09:59:32 +0000

    New util function: processWindowMessages()

--- garnet3d/main/src/priv/base/misc.cpp
+++ garnet3d/main/src/priv/base/misc.cpp
@@ -31,3 +31,39 @@ void GN::sleep( uint32_t microSeconds )
     GN_CASSERT_EX( 0, "Unimplmented" );
 #endif
 }
+
+//
+//
+// -----------------------------------------------------------------------------
+void GN::processWindowMessages( bool * quit )
+{
+#if GN_WINNT
+
+    GN_GUARD;
+
+    if( *quit ) *quit = false;
+
+    MSG msg;
+    while( true )
+    {
+        if( ::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) )
+        {
+            if( WM_QUIT == msg.message )
+            {
+                if( quit ) *quit = true;
+                return;
+            }
+            ::TranslateMessage( &msg );
+            ::DispatchMessage(&msg);
+        }
+        else if( ::IsIconic(msg.hwnd) ) // minimized
+        {
+            ::WaitMessage();
+        }
+        else return; // Idle time!
+    }
+
+    GN_UNGUARD;
+
+#endif
+}
