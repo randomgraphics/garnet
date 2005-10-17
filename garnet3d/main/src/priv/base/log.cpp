e730f4de049373437a61dd4974d2b406cf2ef59b
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-10-17 03:02:28 +0000

    Fix icl build break.

--- garnet3d/main/src/priv/base/log.cpp
+++ garnet3d/main/src/priv/base/log.cpp
@@ -153,7 +153,7 @@ void GN::detail::defaultLogImpl( const LogDesc & desc, const char * msg )
 #if GN_WINNT
         char buf[16384];
         _snprintf( buf, 16384,
-            "%s(%d) : %s : %s\n",
+            "%s(%d) : %s : %s : %s\n",
             file, desc.line,
             cate, levelStr(desc.level).cstr(),
             msg );
