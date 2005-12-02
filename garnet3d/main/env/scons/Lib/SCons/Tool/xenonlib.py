06e754902447dfd984d1cea1640772732f6a58ff
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2005-12-02 03:24:14 +0000

    preliminary support to xenon compiler.

---
+++ garnet3d/main/env/scons/Lib/SCons/Tool/xenonlib.py
@@ -0,0 +1,33 @@
+"""SCons.Tool.xenonlib
+
+Tool-specific initialization for lib (MicroSoft Xbox 360 SDK).
+
+There normally shouldn't be any need to import this module directly.
+It will usually be imported through the generic SCons.Tool.Tool()
+selection method.
+
+"""
+
+import SCons.Defaults
+import SCons.Tool
+import SCons.Tool.xenon
+import SCons.Util
+
+def generate(env):
+    """Add Builders and construction variables for lib to an Environment."""
+    SCons.Tool.createStaticLibBuilder(env)
+
+    include_path, lib_path, exe_path = SCons.Tool.xenon.get_xenon_paths(env)
+
+    # since other tools can set this, we just make sure that the
+    # relevant stuff from MSVS is in there somewhere.
+    env.PrependENVPath('PATH', exe_path)
+
+    env['AR']          = 'lib'
+    env['ARFLAGS']     = SCons.Util.CLVar('/nologo')
+    env['ARCOM']       = "${TEMPFILE('$AR $ARFLAGS /OUT:$TARGET $SOURCES')}"
+    env['LIBPREFIX']   = ''
+    env['LIBSUFFIX']   = '.lib'
+
+def exists(env):
+    return SCons.Tool.xenon.exists(env)
