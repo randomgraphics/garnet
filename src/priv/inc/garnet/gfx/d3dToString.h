9d11deb306e5feceb570d6320b3f94449f65ca09
chenli <chenlirandom@users.noreply.github.com>
2005-12-05 07:36:34 +0000

    add missing files in last checkin

---
+++ src/priv/inc/garnet/gfx/d3dToString.h
@@ -0,0 +1,20 @@
+#ifndef __GN_GFX_D3DTOSTRING_H__
+#define __GN_GFX_D3DTOSTRING_H__
+// *****************************************************************************
+//! \file    gfx/d3dToString.h
+//! \brief   Convert D3D tag to string
+//! \author  chenlee (2005.12.5)
+// *****************************************************************************
+
+namespace GN { namespace gfx
+{
+    //!
+    //! D3DFORMAT to string. Return "INVALID D3DFORMAT" if failed.
+    //!
+    const char * D3DFORMAT2Str( int );
+}}
+
+// *****************************************************************************
+//                           End of d3dToString.h
+// *****************************************************************************
+#endif // __GN_GFX_D3DTOSTRING_H__
