bd3ef00376e5b1ae277762590edbbfda7769c39d
chen <chenlirandom@users.noreply.github.com>
2005-12-07 14:55:04 +0000

    "Remove unneccessary inl files"

--- src/priv/gfx/d3d/d3dDrawMgr.cpp
+++ src/priv/gfx/d3d/d3dDrawMgr.cpp
@@ -1,8 +1,5 @@
 #include "pch.h"
 #include "d3dRenderer.h"
-#if !GN_ENABLE_INLINE
-#include "d3dDrawMgr.inl"
-#endif
 #include "d3dTexture.h"
 #include "d3dFont.h"
 #include "d3dQuad.h"
@@ -504,4 +501,65 @@ bool GN::gfx::D3DRenderer::handleDeviceLost()
 
     GN_UNGUARD;
 #endif
+}
+
+//
+//
+// -----------------------------------------------------------------------------
+GN_INLINE void GN::gfx::D3DRenderer::applyDrawState()
+{
+    GN_GUARD_SLOW;
+
+    if( 0 == mDrawState.dirtyFlags.u32 ) return;
+
+    if( mDrawState.dirtyFlags.vtxBinding )
+    {
+        applyVtxBinding( mDrawState.vtxBinding );
+    }
+
+    if( mDrawState.dirtyFlags.vtxBufs )
+    {
+        applyVtxBuffers();
+    }
+
+    applyShader(
+        mDrawState.vtxShader.get(), !!mDrawState.dirtyFlags.vtxShader,
+        mDrawState.pxlShader.get(), !!mDrawState.dirtyFlags.pxlShader );
+
+    // switch state buffer, and clear dirty flags
+    mDrawState.dirtyFlags.u32 = 0;
+
+    // apply render parameters
+    if( !getRpDirtySet().empty() ) applyRenderParameters();
+
+    GN_UNGUARD_SLOW;
+}
+
+//
+//
+// -----------------------------------------------------------------------------
+GN_INLINE void GN::gfx::D3DRenderer::resizeAutoDepthBuffer( const Vector2<uint32_t> & sz )
+{
+    GN_GUARD_SLOW;
+
+    // Check surface size
+    if( sz.x > mAutoDepthSize.x || sz.y > mAutoDepthSize.y || NULL == mAutoDepth )
+    {
+        // release old buffer
+        safeRelease( mAutoDepth );
+
+        // create a new buffer
+        AutoComPtr<IDirect3DSurface9> newSurf;
+        GN_DX_CHECK_R( mDevice->CreateDepthStencilSurface(
+            sz.x, sz.y,
+            mPresentParameters.AutoDepthStencilFormat,
+            mPresentParameters.MultiSampleType, mPresentParameters.MultiSampleQuality,
+            mPresentParameters.Flags & D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL,
+            &mAutoDepth, 0 ) );
+    }
+
+    // update D3D depth surface
+    GN_DX_CHECK_R( mDevice->SetDepthStencilSurface( mAutoDepth ) );
+
+    GN_UNGUARD_SLOW;
 }
--- src/priv/gfx/d3d/d3dTextureMgr.cpp
+++ src/priv/gfx/d3d/d3dTextureMgr.cpp
@@ -1,9 +1,6 @@
 #include "pch.h"
 #include "d3dRenderer.h"
 #include "d3dTexture.h"
-#if !GN_ENABLE_INLINE
-#include "d3dTextureMgr.inl"
-#endif
 
 // *****************************************************************************
 // interface functions
@@ -106,4 +103,64 @@ bool GN::gfx::D3DRenderer::textureDeviceRestore()
     return true;
 
     //GN_UNGUARD;
+}
+
+// *****************************************************************************
+// private functions
+// *****************************************************************************
+
+//
+//
+// ------------------------------------------------------------------------
+GN_INLINE void
+GN::gfx::D3DRenderer::updateTextureFilters( uint32_t stage, const D3DTEXTUREFILTERTYPE * filters )
+{
+    GN_ASSERT( stage < MAX_TEXTURE_STAGES );
+
+    // update texture filters
+
+    if( *filters != mTexParameters[stage].min )
+    {
+        mTexParameters[stage].min = *filters;
+        GN_DX_CHECK( mDevice->SetSamplerState( stage, D3DSAMP_MINFILTER, *filters ) );
+    }
+    ++filters;
+    if( *filters != mTexParameters[stage].mag )
+    {
+        mTexParameters[stage].mag = *filters;
+        GN_DX_CHECK( mDevice->SetSamplerState( stage, D3DSAMP_MAGFILTER, *filters ) );
+    }
+    ++filters;
+    if( *filters != mTexParameters[stage].mip )
+    {
+        mTexParameters[stage].mip = *filters;
+        GN_DX_CHECK( mDevice->SetSamplerState( stage, D3DSAMP_MIPFILTER, *filters ) );
+    }
+}
+
+//
+//
+// ------------------------------------------------------------------------
+GN_INLINE void
+GN::gfx::D3DRenderer::updateTextureWraps( uint32_t stage, const D3DTEXTUREADDRESS * strq )
+{
+    GN_ASSERT( stage < MAX_TEXTURE_STAGES );
+
+    if( *strq != mTexParameters[stage].s )
+    {
+        mTexParameters[stage].s = *strq;
+        mDevice->SetSamplerState( stage, D3DSAMP_ADDRESSU, *strq );
+    }
+    ++strq;
+    if( *strq != mTexParameters[stage].t )
+    {
+        mTexParameters[stage].t = *strq;
+        mDevice->SetSamplerState( stage, D3DSAMP_ADDRESSV, *strq );
+    }
+    ++strq;
+    if( *strq != mTexParameters[stage].r )
+    {
+        mTexParameters[stage].r = *strq;
+        mDevice->SetSamplerState( stage, D3DSAMP_ADDRESSW, *strq );
+    }
 }
