8db124d33f3ec74e545960f38666bb707166f9c4
chen <chenlirandom@users.noreply.github.com>
2006-02-22 16:18:07 +0000

    - update todo list - refine fox header wrapper

--- src/extern/inc/foxAll.h
+++ src/extern/inc/foxAll.h
@@ -8,29 +8,33 @@
 
 #ifndef _XBOX_VER
 
-// disable vc8.0 CRT warnings
-#if _MSC_VER >= 1400
-#ifndef _CRT_SECURE_NO_DEPRECATE
-#define _CRT_SECURE_NO_DEPRECATE
-#endif
-#ifndef _CRT_NONSTDC_NO_DEPRECATE
-#define _CRT_NONSTDC_NO_DEPRECATE
-#endif
-#endif
-
-#if defined(_WIN32) && !defined(WIN32)
-#define WIN32
+#if defined(_MSC_VER) && !defined(__ICL)
+#pragma warning(disable:4244) // 从“int”转换到“FX::FXshort”，可能丢失数据
 #endif
 
 #if defined(__ICL)
 #pragma warning(disable:193) // zero used for undefined preprocessing identifier
 #endif
 
-#if defined(_MSC_VER) && !defined(__ICL)
-#pragma warning(disable:4244) // 从“int”转换到“FX::FXshort”，可能丢失数据
+// disable vc8.0 CRT secure and STDC warnings
+#if _MSC_VER >= 1400
+#ifndef _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE
+#define _GN_UNDEF_CRT_SECURE_NO_DEPRECATE
+#endif
+#ifndef _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _GN_UNDEF_CRT_NONSTDC_NO_DEPRECATE
+#endif
 #endif
 
-// we have zlib h
+// define WIN32
+#if defined(_WIN32) && !defined(WIN32)
+#define WIN32
+#define _GN_UNDEF_WIN32
+#endif
+
+// define HAVE_ZLIB_H
 #ifndef HAVE_ZLIB_H
 #define HAVE_ZLIB_H
 #define _GN_UNDEF_HAVE_ZLIB_H
@@ -38,17 +42,36 @@
 
 #include "fox/fx.h"
 
-// undefine HAVE_ZLIB_H
-#ifdef _GN_UNDEF_HAVE_ZLIB_H
-#undef HAVE_ZLIB_H
+// undef _CRT_SECURE_NO_DEPRECATE
+#ifdef _GN_UNDEF_CRT_SECURE_NO_DEPRECATE
+#undef _CRT_SECURE_NO_DEPRECATE
+#undef _GN_UNDEF_CRT_SECURE_NO_DEPRECATE
 #endif
 
-#if defined(__ICL)
-#pragma warning(default:193)
+// undef _CRT_NONSTDC_NO_DEPRECATE
+#ifdef _GN_UNDEF_CRT_NONSTDC_NO_DEPRECATE
+#undef _CRT_NONSTDC_NO_DEPRECATE
+#undef _GN_UNDEF_CRT_NONSTDC_NO_DEPRECATE
+#endif
+
+// undef WIN32
+#ifdef _GN_UNDEF_WIN32
+#undef WIN32
+#undef _GN_UNDEF_WIN32
+#endif
+
+// undef HAVE_ZLIB_H
+#ifdef _GN_UNDEF_HAVE_ZLIB_H
+#undef HAVE_ZLIB_H
+#undef _GN_UNDEF_HAVE_ZLIB_H
 #endif
 
 #if defined(_MSC_VER) && !defined(__ICL)
 #pragma warning(default:4244)
+#endif
+
+#if defined(__ICL)
+#pragma warning(default:193)
 #endif
 
 #endif // !_XBOX_VER
