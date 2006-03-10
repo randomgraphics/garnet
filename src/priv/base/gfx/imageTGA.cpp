2df497d7f4f73ec539ad67a2ecc83b32486043f0
chen <chenlirandom@users.noreply.github.com>
2006-03-10 17:06:25 +0000

    - Fix memleak in resource manager; add UT to prevent it from happening again - Fix D3D quad renderer runtime error on GF3 TI 4200 - refine TGA image loader - refine GNoglInfo

--- src/priv/base/gfx/imageTGA.cpp
+++ src/priv/base/gfx/imageTGA.cpp
@@ -1,6 +1,30 @@
 #include "pch.h"
 #include "imageTGA.h"
 
+//
+// TGA image header
+//
+#pragma pack(push,1)
+struct TGA_HEADER
+{
+    uint8_t  identsize;          //!< size of ID field that follows 18 uint8_t header (0 usually)
+    uint8_t  colourmaptype;      //!< type of colour map 0=none, 1=has palette
+    uint8_t  imagetype;          //!< type of image 0=none,1=indexed,2=rgb,3=grey,(9,10,11,32,33)=rle packed
+
+    uint16_t colourmapstart;     //!< first colour map entry in palette
+    uint16_t colourmaplength;    //!< number of colours in palette
+    uint8_t  colourmapbits;      //!< number of bits per palette entry 15,16,24,32
+
+    uint16_t xstart;             //!< image x origin
+    uint16_t ystart;             //!< image y origin
+    uint16_t width;              //!< image width in pixels
+    uint16_t height;             //!< image height in pixels
+    uint8_t  bits;               //!< image bits per pixel 8,16,24,32
+    uint8_t  descriptor;         //!< image descriptor bits (vh flip bits)
+};
+#pragma pack(pop)
+GN_CASSERT( sizeof(TGA_HEADER) == 18 );
+
 // *****************************************************************************
 // local functions
 // *****************************************************************************
@@ -71,7 +95,7 @@ static bool sReadRLERGBImage(
 
     const uint8_t * end = src + srcSize;
 
-    size_t bppSrc = header.bits / 8;
+    size_t srcBpp = header.bits / 8;
 
     size_t numPixels = header.width * header.height;
 
@@ -91,7 +115,7 @@ static bool sReadRLERGBImage(
         if( chunk & 0x80 )
         {
             // read RLE chunk
-            CHECK_SRC_SIZE( bppSrc );
+            CHECK_SRC_SIZE( srcBpp );
             switch( header.bits )
             {
                 case 16: sCopyPixel5551( src, 0, dst, 2, runSize ); dst += runSize*2; break;
@@ -101,12 +125,12 @@ static bool sReadRLERGBImage(
                     GN_ERROR( "unsupport uncompressed RGB TGA image bits: %d", header.bits );
                     return false;
             }
-            src += bppSrc;
+            src += srcBpp;
         }
         else
         {
             // read RAW chunk
-            CHECK_SRC_SIZE( bppSrc * runSize );
+            CHECK_SRC_SIZE( srcBpp * runSize );
             switch( header.bits )
             {
                 case 16: sCopyPixel5551( src, 2, dst, 2, runSize ); dst += runSize*2; break;
@@ -116,7 +140,7 @@ static bool sReadRLERGBImage(
                     GN_ERROR( "unsupport uncompressed RGB TGA image bits: %d", header.bits );
                     return false;
             }
-            src += bppSrc * runSize;
+            src += srcBpp * runSize;
         }
         i += runSize;
     }
@@ -145,32 +169,32 @@ bool TGAReader::checkFormat( GN::File & fp )
 {
     GN_GUARD;
 
-    TGA_HEADER mHeader;
+    TGA_HEADER header;
 
-    // read TGA mHeader
+    // read TGA header
     if( !fp.seek( 0, GN::FSEEK_SET ) ) return false;
-    if( sizeof(mHeader) != fp.read( &mHeader, sizeof(mHeader) ) ) return false;
+    if( sizeof(TGA_HEADER) != fp.read( &header, sizeof(TGA_HEADER) ) ) return false;
 
-    // check mHeader fields
-    return (  0 == mHeader.colourmaptype
-          ||  1 == mHeader.colourmaptype )
-        && (  0 == mHeader.imagetype
-          ||  1 == mHeader.imagetype
-          ||  2 == mHeader.imagetype
-          ||  3 == mHeader.imagetype
-          ||  9 == mHeader.imagetype
-          || 10 == mHeader.imagetype
-          || 11 == mHeader.imagetype
-          || 32 == mHeader.imagetype
-          || 33 == mHeader.imagetype )
-        && (  0 == mHeader.colourmapbits
-          || 15 == mHeader.colourmapbits
-          || 16 == mHeader.colourmapbits
-          || 24 == mHeader.colourmapbits
-          || 32 == mHeader.colourmapbits )
-        && ( 16 == mHeader.bits
-          || 24 == mHeader.bits
-          || 32 == mHeader.bits );
+    // check header fields
+    return (  0 == header.colourmaptype
+          ||  1 == header.colourmaptype )
+        && (  0 == header.imagetype
+          ||  1 == header.imagetype
+          ||  2 == header.imagetype
+          ||  3 == header.imagetype
+          ||  9 == header.imagetype
+          || 10 == header.imagetype
+          || 11 == header.imagetype
+          || 32 == header.imagetype
+          || 33 == header.imagetype )
+        && (  0 == header.colourmapbits
+          || 15 == header.colourmapbits
+          || 16 == header.colourmapbits
+          || 24 == header.colourmapbits
+          || 32 == header.colourmapbits )
+        && ( 16 == header.bits
+          || 24 == header.bits
+          || 32 == header.bits );
 
     GN_UNGUARD;
 }
@@ -185,61 +209,60 @@ bool TGAReader::readHeader(
 
     GN_ASSERT( i_buf && i_size );
 
-    // copy TGA mHeader
-    if( i_size <= sizeof(mHeader) )
+    const TGA_HEADER & header = *(const TGA_HEADER*)i_buf;
+
+    // copy TGA header
+    if( i_size <= sizeof(TGA_HEADER) )
     {
-        GN_ERROR( "File size is too small to hold TGA file mHeader." );
+        GN_ERROR( "File size is too small to hold TGA file header." );
         return false;
     }
-    ::memcpy( &mHeader, i_buf, sizeof(mHeader) );
 
     // What can we handle
-    if( mHeader.imagetype != 2 && mHeader.imagetype != 3 && mHeader.imagetype != 10 )
+    if( header.imagetype != 2 && header.imagetype != 3 && header.imagetype != 10 )
     {
         GN_ERROR( "We can only handle TGA image type 2(rgb), 3(grey) and 10(rle-rgb).");
         return false;
     }
-    if( mHeader.colourmaptype != 0 && mHeader.colourmaptype != 1 )
+    if( header.colourmaptype != 0 && header.colourmaptype != 1 )
     {
         GN_ERROR( "We can only handle colour map types of 0 and 1." );
         return false;
     }
 
     // point mSrc to the start of image data
-    size_t skipOver = sizeof(mHeader)
-                    + mHeader.identsize
-                    + mHeader.colourmaptype * mHeader.colourmaplength * mHeader.colourmapbits / 8;
-    if( i_size < skipOver )
+    size_t minimalSize = sizeof(TGA_HEADER)
+                       + header.identsize
+                       + header.colourmaptype * header.colourmaplength * header.colourmapbits / 8;
+    if( i_size < minimalSize )
     {
-        GN_ERROR( "File size is not large enough: minimum(%d) actual(%d).", skipOver, i_size );
+        GN_ERROR( "File size is not large enough: minimum(%d) actual(%d).", minimalSize, i_size );
         return false;
     }
-    mSrc  = i_buf + skipOver;
-    mSize = i_size - skipOver;
 
     // determine image format
-    if( 2 == mHeader.imagetype || 10 == mHeader.imagetype )
+    if( 2 == header.imagetype || 10 == header.imagetype )
     {   // RGB image
-        switch( mHeader.bits )
+        switch( header.bits )
         {
             case 16 : o_desc.format = GN::gfx::FMT_BGRA_5_5_5_1_UNORM; mOutputBytesPerPixel = 2; break;
             case 24 : o_desc.format = GN::gfx::FMT_BGRA_8_8_8_8_UNORM; mOutputBytesPerPixel = 4; break;
             case 32 : o_desc.format = GN::gfx::FMT_BGRA_8_8_8_8_UNORM; mOutputBytesPerPixel = 4; break;
             default :
-                GN_ERROR( "unsupport/invalid RGB image bits: %d.", mHeader.bits );
+                GN_ERROR( "unsupport/invalid RGB image bits: %d.", header.bits );
                 return false;
         }
     }
     else
     {   // grayscale image
-        GN_ASSERT( 3 == mHeader.imagetype );
-        switch( mHeader.bits )
+        GN_ASSERT( 3 == header.imagetype );
+        switch( header.bits )
         {
             case  8 : o_desc.format = GN::gfx::FMT_L_8_UNORM; mOutputBytesPerPixel = 1; break;
             case 16 : o_desc.format = GN::gfx::FMT_L_16_UNORM; mOutputBytesPerPixel = 2; break;
             case 32 : o_desc.format = GN::gfx::FMT_L_32_UNORM; mOutputBytesPerPixel = 4; break;
             default :
-                GN_ERROR( "unsupport/invalid grey image bits: %d.", mHeader.bits );
+                GN_ERROR( "unsupport/invalid grey image bits: %d.", header.bits );
                 return false;
         }
     }
@@ -247,15 +270,17 @@ bool TGAReader::readHeader(
     // update o_desc
 	o_desc.setFaceAndLevel( 1, 1 ); // 2D image
     GN::gfx::MipmapDesc & m = o_desc.getMipmap( 0, 0 );
-    m.width      = mHeader.width;
-    m.height     = mHeader.height;
+    m.width      = header.width;
+    m.height     = header.height;
     m.depth      = 1;
-    m.rowPitch   = mHeader.width * mOutputBytesPerPixel;
-    m.slicePitch = m.rowPitch * mHeader.height;
+    m.rowPitch   = header.width * mOutputBytesPerPixel;
+    m.slicePitch = m.rowPitch * header.height;
     m.levelPitch = m.slicePitch;
     GN_ASSERT( o_desc.valid() );
 
     // success
+    mImageSrc  = i_buf;
+    mImageSize = i_size;
     return true;
 
     GN_UNGUARD;
@@ -274,11 +299,17 @@ bool TGAReader::readImage( void * o_data )
         return false;
     }
 
-    GN_ASSERT( mSrc );
-    const uint8_t * src = mSrc;
+    GN_ASSERT( mImageSrc );
+
+    const TGA_HEADER & header = *(const TGA_HEADER*)mImageSrc;
+    size_t skipOver = sizeof(TGA_HEADER)
+                   + header.identsize
+                   + header.colourmaptype * header.colourmaplength * header.colourmapbits / 8;
+    const uint8_t * src = mImageSrc + skipOver;
     uint8_t * dst = (uint8_t*)o_data;
-    size_t bppSrc = mHeader.bits / 8;
-    size_t numPixels = mHeader.width * mHeader.height;
+    size_t srcSize = mImageSize - skipOver;
+    size_t srcBpp = header.bits / 8;
+    size_t numPixels = header.width * header.height;
 
     // check image descriptor
     union ImageDescriptor
@@ -299,33 +330,33 @@ bool TGAReader::readImage( void * o_data )
         };
     };
     ImageDescriptor id;
-    id.u8 = mHeader.descriptor;
+    id.u8 = header.descriptor;
 
     // read image to temporary buffer, if image is flipped and/or interleaved.
     std::vector<uint8_t> tempBuf;
     if( 0 != id.interleaved || 0 == id.flip )
     {
-        tempBuf.resize( numPixels * bppSrc );
+        tempBuf.resize( numPixels * srcBpp );
         dst = &tempBuf[0];
     }
 
-    switch( mHeader.imagetype )
+    switch( header.imagetype )
     {
         // uncompressed RGB image
         case 2 :
         {
-            if( mSize < numPixels * bppSrc )
+            if( srcSize < numPixels * srcBpp )
             {
                 GN_ERROR( "incomplete image data." );
                 return false;
             }
-            switch( mHeader.bits )
+            switch( header.bits )
             {
                 case 16: sCopyPixel5551( src, 2, dst, 2, numPixels ); break;
                 case 24: sCopyPixel888( src, 3, dst, 4, numPixels ); break;
                 case 32: sCopyPixel8888( src, 4, dst, 4, numPixels ); break;
                 default:
-                    GN_ERROR( "unsupport uncompressed RGB TGA image bits: %d", mHeader.bits );
+                    GN_ERROR( "unsupport uncompressed RGB TGA image bits: %d", header.bits );
                     return false;
             }
             break;
@@ -334,18 +365,18 @@ bool TGAReader::readImage( void * o_data )
         // uncompressed grayscale image
         case 3:
         {
-            if( mSize < numPixels * bppSrc )
+            if( srcSize < numPixels * srcBpp )
             {
                 GN_ERROR( "incomplete image data." );
                 return false;
             }
-            switch( mHeader.bits )
+            switch( header.bits )
             {
                 case  8: memcpy( dst, src, numPixels*1 ); break;
                 case 16: memcpy( dst, src, numPixels*2 ); break;
                 case 32: memcpy( dst, src, numPixels*4 ); break;
                 default:
-                    GN_ERROR( "unsupport uncompressed grayscale TGA image bits: %d", mHeader.bits );
+                    GN_ERROR( "unsupport uncompressed grayscale TGA image bits: %d", header.bits );
                     return false;
             }
             break;
@@ -353,11 +384,11 @@ bool TGAReader::readImage( void * o_data )
 
         // RLE-compressed RGB image
         case 10 :
-            if( !sReadRLERGBImage( mHeader, src, mSize, dst ) ) return false;
+            if( !sReadRLERGBImage( header, src, srcSize, dst ) ) return false;
             break;
 
         default:
-            GN_ERROR( "unsupport TGA image type : %d", mHeader.imagetype );
+            GN_ERROR( "unsupport TGA image type : %d", header.imagetype );
             return false;
     }
 
@@ -368,10 +399,10 @@ bool TGAReader::readImage( void * o_data )
     if( 0 == id.flip )
     {
         dst = (uint8_t*)o_data;
-        size_t rowPitch = mHeader.width * mOutputBytesPerPixel;
-        for( size_t y = 0; y < mHeader.height; ++y )
+        size_t rowPitch = header.width * mOutputBytesPerPixel;
+        for( size_t y = 0; y < header.height; ++y )
         {
-            memcpy( &dst[rowPitch*(mHeader.height-y-1)], &tempBuf[rowPitch*y], rowPitch );
+            memcpy( &dst[rowPitch*(header.height-y-1)], &tempBuf[rowPitch*y], rowPitch );
         }
     }
 
--- src/priv/base/gfx/imageTGA.h
+++ src/priv/base/gfx/imageTGA.h
@@ -6,40 +6,13 @@
 //! \author  chenlee (2005.6.2)
 // *****************************************************************************
 
-#pragma pack(push,1)
-
-//!
-//! TGA image header
-//!
-struct TGA_HEADER
-{
-    uint8_t  identsize;          //!< size of ID field that follows 18 uint8_t header (0 usually)
-    uint8_t  colourmaptype;      //!< type of colour map 0=none, 1=has palette
-    uint8_t  imagetype;          //!< type of image 0=none,1=indexed,2=rgb,3=grey,(9,10,11,32,33)=rle packed
-
-    uint16_t colourmapstart;     //!< first colour map entry in palette
-    uint16_t colourmaplength;    //!< number of colours in palette
-    uint8_t  colourmapbits;      //!< number of bits per palette entry 15,16,24,32
-
-    uint16_t xstart;             //!< image x origin
-    uint16_t ystart;             //!< image y origin
-    uint16_t width;              //!< image width in pixels
-    uint16_t height;             //!< image height in pixels
-    uint8_t  bits;               //!< image bits per pixel 8,16,24,32
-    uint8_t  descriptor;         //!< image descriptor bits (vh flip bits)
-};
-
-#pragma pack(pop)
-GN_CASSERT( sizeof(TGA_HEADER) == 18 );
-
 //!
 //! tga image reader.
 //!
 class TGAReader
 {
-    TGA_HEADER      mHeader;
-    const uint8_t * mSrc;
-    size_t          mSize; // image data size in bytes;
+    const uint8_t * mImageSrc;
+    size_t          mImageSize;
     size_t          mOutputBytesPerPixel;
 
 public:
@@ -47,7 +20,7 @@ public:
     //!
     //! constructor
     //!
-    TGAReader() : mSrc(0) {}
+    TGAReader() : mImageSrc(0) {}
 
     //!
     //! destructor
