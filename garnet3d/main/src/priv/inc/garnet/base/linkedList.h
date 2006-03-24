54ea8b0370ca1df3d45e6eae3e0d84137d6f6c08
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2006-03-24 08:52:21 +0000

    double-linked list template (wip)

---
+++ garnet3d/main/src/priv/inc/garnet/base/linkedList.h
@@ -0,0 +1,48 @@
+#ifndef __GN_BASE_LINKEDLIST_H__
+#define __GN_BASE_LINKEDLIST_H__
+// *****************************************************************************
+//! \file    base/linkedList.h
+//! \brief   Templates of double-linked list container
+//! \author  chenlee (2006.3.24)
+// *****************************************************************************
+
+namespace GN
+{
+    //!
+    //! Templates of double-linked list item
+    //!
+    template<class T>
+    struct DoubleLinkedItem
+    {
+        T * prev;
+        T * next;
+    };
+
+    //!
+    //! Templates of double-linked list container
+    //!
+    template<class T>
+    class DoubleLinkedList : public NoCopy
+    {
+        typedef DoubleLinkedItem<T> ItemType;
+
+        ItemType *mHead, *mTail;
+
+    public:
+
+        //!
+        //! constructor
+        //!
+        DoubleLinkedList() : mHead(0), mTail(0) {}
+
+        //!
+        //! Destructor
+        //!
+        virtual ~DoubleLinkedList() {}
+    };
+}
+
+// *****************************************************************************
+//                           End of linkedList.h
+// *****************************************************************************
+#endif // __GN_BASE_LINKEDLIST_H__
