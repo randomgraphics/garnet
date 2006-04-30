4932e8a8c7e9df24182efd48559eecab384c26eb
chenli <chenlirandom@users.noreply.github.com>
2006-04-30 13:04:54 +0000

    refine msvc build script

--- msvc/build.cmd
+++ msvc/build.cmd
@@ -1,6 +1,10 @@
 @echo off
 del /s GN*.vcproj 2>nul
-for %%c in (vc80 vc80-x64 xenon) do (
-    echo call scons -U . variant=all compiler=%%c -j1 --no-cache %*
-    call scons -U . variant=all compiler=%%c -j1 --no-cache %*
+for %%c in (vc80 vc71 xenon) do (
+	for %%d in (x86 x64 xenon) do (
+		for %%o in (mswin xenon) do (
+		    echo call scons -U . variant=all compiler=%%c os=%%o cpu=%%d -j1 --no-cache %*
+    		call scons -U . variant=all compiler=%%c os=%%o cpu=%%d -j1 --no-cache %*
+    	)
+    )
 )
