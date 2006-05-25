04046c88fd3ef07996326db96599d6a5b825bfca
chenli <chenli@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2006-05-25 03:37:48 +0000

    reactivate ATI depth texture bug.

--- garnet3d/main/src/priv/sample/depthTexture/depthTexture.cpp
+++ garnet3d/main/src/priv/sample/depthTexture/depthTexture.cpp
@@ -36,8 +36,8 @@ public:
     {
         Renderer & r = gRenderer;
 
-        uint32_t w = 512;
-        uint32_t h = 512;
+        uint32_t w = 1024;
+        uint32_t h = 1024;
 
         // create color texture
         mColor.attach( r.create2DTexture( w, h, 1, FMT_DEFAULT, TEXUSAGE_RENDER_TARGET ) );
