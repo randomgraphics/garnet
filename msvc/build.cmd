04c1a3fc6fc0c1dc7ffad02610e6f32559917073
chenli <chenlirandom@users.noreply.github.com>
2006-06-28 09:07:40 +0000

    minor refine.

--- msvc/build.cmd
+++ msvc/build.cmd
@@ -1,12 +1,20 @@
 @echo off
 del /s GN*.vcproj 2>nul
-for %%c in (vc80 vc71 xenon) do (
-	for %%d in (x86 x64 ppc) do (
-		for %%o in (mswin xenon) do (
+for %%c in (vc80 vc71) do (
+	for %%d in (x86 x64) do (
+		for %%o in (mswin) do (
 			for %%v in (debug profile retail stdbg stprof stret) do (
 				echo call scons -U . variant=%%v compiler=%%c os=%%o cpu=%%d -j1 --no-cache %*
 				call scons -U . variant=%%v compiler=%%c os=%%o cpu=%%d -j1 --no-cache %*
 			)
 		)
 	)
+)
+
+REM
+REM Generate vc projects for xenon build
+REM
+for %%v in (debug profile retail stdbg stprof stret) do (
+	echo call scons -U . variant=%%v compiler=xenon os=xenon cpu=ppc -j1 --no-cache %*
+	call scons -U . variant=%%v compiler=xenon os=xenon cpu=ppc -j1 --no-cache %*
 )
