53174cc5411b69376a9f41e1aca77c5ac57bc160
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2007-04-09 00:34:25 +0000

    implement thread and semaphore class

--- garnet3d/main/src/priv/inc/garnet/base/thread.h
+++ garnet3d/main/src/priv/inc/garnet/base/thread.h
@@ -14,11 +14,29 @@ namespace GN
     extern const float INFINITE_TIME; // = 1.0e38f;
 
     ///
+    /// thread priority
+    ///
+    enum ThreadPriority
+    {
+        TP_REALTIME,  ///< The highest priority, for time-critical task only.
+        TP_HIGH,      ///< high priority
+        TP_NORMAL,    ///< normal priority, suitable for most of application.
+        TP_LOW,       ///< low priority
+        TP_IDLE,      ///< The lowest priority
+        NUM_THREAD_PRIORITIES, ///< number of thread priorities
+    };
+
+    ///
+    /// thread procedure functor
+    ///
+    typedef Delegate1<UInt32,void*> ThreadProcedure;
+
+    ///
     /// Mutex lock, the lightest sync object.
     ///
     class Mutex
     {
-        UInt32 mInternal[8]; ///< store platform dependata data
+        UInt32 mInternal[16]; ///< store platform dependata data here, no runtime heap allocation
 
     public:
 
@@ -35,25 +53,22 @@ namespace GN
     };
 
     ///
-    /// Semaphore
-    ///
-    class Semaphore
+    /// Abstract semaphore interface
+    ///    
+    struct Semaphore : public NoCopy
     {
+        virtual bool wait( float seconds = INFINITE_TIME ) = 0; ///< block calling thread, until the semaphore is available.
+        virtual void wake() = 0; ///< wake up one thread that is waiting for this semaphore.
+
+        /// \name aliases for P/V operations
+        //@{
+        bool P( float seconds = INFINITE_TIME ) { return wait( seconds ); }
+        void V() { return wake(); }
+        //@}
     };
 
     ///
-    /// thread priority
-    ///
-    enum ThreadPriority
-    {
-        TP_REALTIME, ///< The highest priority, for time-critical task only.
-        TP_HIGH,     ///< high priority
-        TP_NORMAL,   ///< normal priority, suitable for most of application.
-        TP_LOW,      ///< low priority
-    };
-
-    ///
-    /// thread interface
+    /// abstract thread interface
     ///
     struct Thread : public NoCopy
     {
@@ -61,18 +76,18 @@ namespace GN
         //@{
         virtual ThreadPriority getPriority() const = 0;
         virtual void           setPriority( ThreadPriority ) = 0;
+        virtual bool           isCurrentThread() const = 0;
         //@}
 
         /// \name thread operations
         //@{
         virtual void suspend() = 0;
-        virtual void sleep( float seconds ) = 0;
         virtual void resume() = 0; //< resume suspended and sleeping thread.
 
         ///
         /// wait for termination of the thread.
         ///
-        virtual bool waitForTermination( float seconds = INFINITE_TIME ) = 0;
+        virtual bool waitForTermination( float seconds = INFINITE_TIME, UInt32 * threadProcReturnValue = 0 ) = 0;
 
         ///
         /// signal of thread termination
@@ -82,17 +97,34 @@ namespace GN
         //@}
     };
 
-    ///
-    /// thread procedure functor
-    ///
-    typedef Delegate1<void,void*> ThreadProcedure;
+    //@{
 
-    ///
-    /// create a new thread
-    ///
+    Semaphore * createSemaphore( size_t maxcount, size_t initialcount, const char * name = 0 );
+
     Thread * createThread(
+        const ThreadProcedure & proc,
+        void * param,
         ThreadPriority priority,
-        const ThreadProcedure & proc );
+        bool initialSuspended = false,
+        const char * name = 0 );
+
+    void sleepCurrentThread( float seconds );
+
+    // Thread * getCurrentThread();
+
+    //@}
+
+    /*// \name atomic operations
+    //@{
+
+    atomread32(...);
+    atomwrite32(...);
+    atominc32(...);
+    atomdec32(...);
+    atomxchg32(...);
+    atomcx32(...);
+
+    //@}*/
 }
 
 // *****************************************************************************
