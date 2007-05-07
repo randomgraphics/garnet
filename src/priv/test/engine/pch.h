caf399de21a30537419087349ad0e8e690921dd7
chenlee <chenlirandom@users.noreply.github.com>
2007-05-07 12:18:27 +0000

    new test application: GNtestEngine

---
+++ src/priv/test/engine/pch.h
@@ -0,0 +1,22 @@
+#ifndef __GN_GFX_PCH_H__
+#define __GN_GFX_PCH_H__
+// *****************************************************************************
+// \file    pch.h
+// \brief   PCH header
+// \author  chenlee (2005.10.9)
+// *****************************************************************************
+
+#include "garnet/GNbase.h"
+#include "garnet/GNcore.h"
+#include "garnet/GNwin.h"
+#include "garnet/GNgfx.h"
+#include "garnet/GNinput.h"
+#include "garnet/GNscene.h"
+#include "garnet/GNutil.h"
+#include "garnet/GNapp.h"
+
+
+// *****************************************************************************
+//                           End of pch.h
+// *****************************************************************************
+#endif // __GN_GFX_PCH_H__
