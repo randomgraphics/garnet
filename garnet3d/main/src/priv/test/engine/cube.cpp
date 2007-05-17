303dadd92255db347d59db36333fe1ed1c496b11
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2007-05-17 08:43:50 +0000

    fix bugs in GN::engine::effect class.

--- garnet3d/main/src/priv/test/engine/cube.cpp
+++ garnet3d/main/src/priv/test/engine/cube.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "cube.h"
+#include "garnet/GNscene.h"
 
 using namespace GN;
 using namespace GN::input;
@@ -8,41 +9,22 @@ using namespace GN::engine;
 
 static GN::Logger * sLogger = GN::getLogger("GN.test.engine.Cube");
 
-static const char * vscode =
-    "uniform float4x4 pvw;\n"
-    "void main( in float4 ipos : POSITION,   \n"
-    "           in float2 itex : TEXCOORD0,  \n"
-    "          out float4 opos : POSITION,   \n"
-    "          out float2 otex : TEXCOORD0 ) \n"
-    "{ \n"
-    "    opos = mul( pvw, ipos ); \n"
-    "    otex = itex; \n"
-    "}";
-
-static const char * pscode =
-    "sampler s0 : register(s0); \n"
-    "float4 main( in float2 itex : TEXCOORD0 ) : COLOR0 \n"
-    "{ \n"
-    "   return tex2D( s0, itex ); \n"
-    "}";
-
 bool TestCube::init()
 {
     EntityManager & em = entityManager();
     RenderEngine & re = renderEngine();
 
-    // create vertex shader
-    vs = createShader( re, SHADER_VS, LANG_D3D_HLSL, vscode, "", "vs1" );
-
-    // create pixel shader
-    ps = createShader( re, SHADER_PS, LANG_D3D_HLSL, pscode, "", "ps1" );
+    // create effect
+    engine::EffectDesc ed;
+    if( !scene::loadFromXmlFile( ed, "media::/cube/cube_on_cube.effect.xml" ) ) return false;
+    if( !eff.init( ed ) ) return false;
 
     // create mesh entity
     mesh = loadMeshEntityFromFile( em, re, "media::/cube/cube.mesh.xml" );
     if( 0 == mesh ) return false;
 
     // create texture entity
-    tex = loadTextureEntityFromFile( em, re, "media::/texture/earth.jpg" );
+    tex = loadTextureEntityFromFile( em, re, "media::/texture/cube1.dds" );
     if( 0 == tex ) return false;
 
     // success
@@ -56,6 +38,8 @@ void TestCube::quit()
 
     deleteEntity( mesh );
     deleteEntity( tex );
+
+    eff.quit();
 }
 
 void TestCube::draw()
@@ -65,21 +49,23 @@ void TestCube::draw()
     // dispose all
     re.disposeAllResources();
 
-    // set uniform
+    // calculate pvw matrix
     Matrix44f proj,view;
     proj.perspectiveD3D( 1.0f, 4.0f/3.0f, 10.0f, 500.0f );
     view.lookAt( Vector3f(100.0f,100.0f,100.0f), Vector3f(0,0,0), Vector3f(0,1.0f,0) );
-    re.setShaderUniform( vs, "pvw", proj*view );
 
+    // load mesh
     engine::Mesh * m = entity2Object<engine::Mesh*>(mesh,0);
 
     // bind context
     DrawContext ctx;
     ctx.resetToDefault();
-    ctx.setVS( (const Shader*)vs );
-    ctx.setPS( (const Shader*)ps );
-    ctx.setTexture( 0, (const Texture*)entity2Texture(tex) );
+    eff.setUniformByName( "pvw", proj*view );
+    eff.setTextureByName( "cube", tex );
+    eff.passBegin( ctx, 0 );
     m->updateContext( ctx );
+    eff.commitChanges();
+    eff.passEnd();
     re.setContext( ctx );
 
     // do draw
