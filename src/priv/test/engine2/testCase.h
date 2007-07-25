1f6ec4adfadb2f96b774e6acdfa9d4be109ac273
chenlee <chenlirandom@users.noreply.github.com>
2007-07-25 14:44:43 +0000

    new test application for engine2

---
+++ src/priv/test/engine2/testCase.h
@@ -0,0 +1,44 @@
+#ifndef __GN_TEST_ENGINE2_TESTCASE_H__
+#define __GN_TEST_ENGINE2_TESTCASE_H__
+// *****************************************************************************
+/// \file
+/// \brief   common test case interface
+/// \author  chen@@CHENLI-HOMEPC (2007.5.7)
+// *****************************************************************************
+
+#include "garnet/GNengine2.h"
+
+using namespace GN::engine2;
+
+///
+/// coming soon ...
+///
+class TestCase
+{
+    RenderEngine  & mRenderEngine;
+
+public:
+
+    ///
+    /// ctor
+    ///
+    TestCase( RenderEngine & re ) : mRenderEngine( re ) {}
+
+    ///
+    /// get engine reference
+    ///
+    RenderEngine & renderEngine() const { return mRenderEngine; }
+
+    //@{
+
+    virtual bool init() = 0;
+    virtual void quit() = 0;
+    virtual void draw() = 0;
+
+    //@}
+};
+
+// *****************************************************************************
+//                                     EOF
+// *****************************************************************************
+#endif // __GN_TEST_ENGINE2_TESTCASE_H__
