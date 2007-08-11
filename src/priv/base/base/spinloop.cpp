9e3f071604f9e382dbab49923eed79fb92f7fe65
chenlee <chenlirandom@users.noreply.github.com>
2007-08-11 07:11:09 +0000

    add missing file in last checkin

---
+++ src/priv/base/base/spinloop.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+void GN::SpinLoop::lock()
+{
+    size_t i = 0;
+    while( 0 != atomCmpXchg32( &mLock, 1, 0 ) )
+    {
+        ++i;
+        if( i > 1000000 )
+        {
+            sleepCurrentThread( 0.0f );
+            i = 0;
+        }
+    }
+}
