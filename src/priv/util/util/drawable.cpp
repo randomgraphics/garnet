010042113cd8473676f5956e996c349f9594134b
chenlee <chenlirandom@users.noreply.github.com>
2009-01-08 05:12:20 +0000

    move function implemetation of drawable class out of header file

--- src/priv/util/util/drawable.cpp
+++ src/priv/util/util/drawable.cpp
@@ -65,3 +65,82 @@ GpuProgramParam * GN::util::createSharedGpuProgramParam( const StrA & name, size
         // not found!
         return new SharedGpp( name, size );
 }
+
+// *****************************************************************************
+// drawable class
+// *****************************************************************************
+
+//
+//
+// -----------------------------------------------------------------------------
+void GN::util::Drawable::draw() const
+{
+    GN_ASSERT( rndr );
+
+    // apply parameters
+    if( rc.gpuProgram )
+    {
+        for( size_t i = 0; i < gpps.size(); ++i )
+        {
+            GpuProgramParam * gpp = gpps[i].get();
+
+            if( gpp )
+            {
+                rc.gpuProgram->setParameter( i, gpp->get(), gpp->size() );
+            }
+        }
+    }
+
+    // bind context
+    rndr->bindContext( rc );
+
+    // do rendering
+    if( rc.idxbuf )
+    {
+        rndr->drawIndexed( prim, numidx, startvtx, minvtxidx, numvtx, startidx );
+    }
+    else
+    {
+        rndr->draw( prim, numvtx, startvtx );
+    }
+}
+
+//
+//
+// -----------------------------------------------------------------------------
+GN::util::Drawable &
+GN::util::Drawable::operator=( const Drawable & rhs )
+{
+    rndr = rhs.rndr;
+    rc = rhs.rc;
+    prim = rhs.prim;
+    numvtx = rhs.numvtx;
+    numidx = rhs.numidx;
+    minvtxidx = rhs.minvtxidx;
+    startidx = rhs.startidx;
+
+    // copy parameters
+    gpps.resize( rhs.gpps.size() );
+    for( size_t i = 0; i < gpps.size(); ++i )
+    {
+        AutoRef<GpuProgramParam> & d = gpps[i];
+        const AutoRef<GpuProgramParam> & s = rhs.gpps[i];
+
+        if( NULL != s )
+        {
+            if( s->shared() )
+            {
+                d = s;
+            }
+            else
+            {
+                // make a clone for private parameter
+                d.attach( createPrivateGpuProgramParam( s->size() ) );
+                d->set( s->get(), s->size() );
+            }
+        }
+    }
+
+    return *this;
+}
+
