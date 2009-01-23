8487464fd8c6200dcef533cfdf1d9cdf98738a4a
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2009-01-23 03:41:04 +0000

    fix StackArray and DynaArray bugs when used with classes with non-trival ctor/dtor

--- garnet3d/main/src/priv/test/ut/base/array.cpp
+++ garnet3d/main/src/priv/test/ut/base/array.cpp
@@ -2,11 +2,75 @@
 
 class StackArrayTest : public CxxTest::TestSuite
 {
+    struct Element
+    {
+        static int count;  ///< number of Element instances
+        static int ctor;   ///< number of calls to ctor
+        static int dtor;   ///< number of calls to dtor
+        static int cctor;  ///< number of calls to copy constructor
+        static int cop;    ///< number of calls to copy operator
+
+        static void clear() { ctor = 0; dtor = 0; cctor = 0; cop = 0; }
+
+        bool constructed;
+        int  i;
+
+        Element() : constructed(true)
+        {
+            ++ctor; ++count;
+        }
+
+        Element( int i_ ) : constructed(true), i(i_)
+        {
+            ++ctor; ++count;
+        }
+
+        Element( const Element & o )
+            : constructed(true)
+            , i(o.i)
+        {
+            if( !o.constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            ++cctor;
+            ++count;
+        }
+
+        ~Element()
+        {
+            if( !constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            constructed = false;
+
+            ++dtor;
+            --count;
+        }
+
+        Element & operator=( const Element & o )
+        {
+            if( !constructed || !o.constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            i = o.i;
+
+            ++cop;
+            return *this;
+        }
+    };
+
 public:
 
     void testAppend()
     {
         using namespace GN;
+
         StackArray<int,2> a;
         a.append( 1 );
         a.append( 2 );
@@ -14,6 +78,15 @@ public:
         TS_ASSERT_EQUALS( 2, a.size() );
         TS_ASSERT_EQUALS( 1, a.front() );
         TS_ASSERT_EQUALS( 2, a.back() );
+
+        Element::clear();
+        StackArray<Element,2> b;
+        TS_ASSERT_EQUALS( 0, Element::count );
+
+        b.append( 1 );
+        b.append( 2 );
+        b.append( 3 ); // should do nothing
+        TS_ASSERT_EQUALS( 2, Element::count );
     }
 
     void testInsert()
@@ -33,6 +106,19 @@ public:
         TS_ASSERT_EQUALS( 4, a[3] );
     }
 
+    void testInsert2()
+    {
+        using namespace GN;
+        StackArray<Element,4> a;
+        a.insert( 0, 1 ); // 1 (insert at head)
+        a.insert( 1, 3 ); // 1, 3 (insert at tail)
+        a.insert( 1, 2 ); // 1, 2, 3 (insert at middle)
+        a.insert( 100, 0 ); // invalid position, should do nothing
+        a.insert( 3, 4 ); // 1, 2, 3, 4 (insert at tail)
+        a.insert( 4, 5 ); // already full, should do nothing
+        TS_ASSERT_EQUALS( 4, a.size() );
+    }
+
     void testErase()
     {
         using namespace GN;
@@ -43,17 +129,17 @@ public:
         a.append( 4 );
 
         // erase middle: 1, 2, 4
-        a.erase( 2 ); 
+        a.erase( 2 );
         TS_ASSERT_EQUALS( 3, a.size() );
         TS_ASSERT_EQUALS( 4, a[2] );
 
         // erase head: 2, 4
-        a.erase( 0 ); 
+        a.erase( 0 );
         TS_ASSERT_EQUALS( 2, a.size() );
         TS_ASSERT_EQUALS( 2, a[0] );
 
         // erase tail: 2
-        a.erase( 1 ); 
+        a.erase( 1 );
         TS_ASSERT_EQUALS( 1, a.size() );
         TS_ASSERT_EQUALS( 2, a[0] );
 
@@ -65,7 +151,7 @@ public:
         // erase to empty
         a.erase( 0 );
         TS_ASSERT_EQUALS( 0, a.size() );
-        
+
         // already empty
         a.erase( 0 );
         TS_ASSERT_EQUALS( 0, a.size() );
@@ -90,33 +176,174 @@ public:
     }
 };
 
+int StackArrayTest::Element::count = 0;
+int StackArrayTest::Element::ctor  = 0;
+int StackArrayTest::Element::dtor  = 0;
+int StackArrayTest::Element::cctor = 0;
+int StackArrayTest::Element::cop = 0;
+
 class DynaArrayTest : public CxxTest::TestSuite
 {
+    struct Element
+    {
+        static int count;  ///< number of Element instances
+        static int ctor;   ///< number of calls to ctor
+        static int dtor;   ///< number of calls to dtor
+        static int cctor;  ///< number of calls to copy constructor
+        static int cop;    ///< number of calls to copy operator
+
+        static void clear() { ctor = 0; dtor = 0; cctor = 0; cop = 0; }
+
+        bool constructed;
+
+        Element() : constructed(true)
+        {
+            ++ctor; ++count;
+        }
+
+        Element( const Element & o )
+            :constructed(true)
+        {
+            if( !o.constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            ++cctor;
+            ++count;
+        }
+
+        ~Element()
+        {
+            if( !constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            constructed = false;
+
+            ++dtor;
+            --count;
+        }
+
+        Element & operator=( Element & o )
+        {
+            if( !constructed || !o.constructed )
+            {
+                GN_UNEXPECTED();
+            }
+
+            ++cop;
+            return *this;
+        }
+    };
+
 public:
 
     void testDefautlCtor()
     {
         using namespace GN;
-        DynaArray<int> a;
+        DynaArray<Element> a;
 
         TS_ASSERT( a.empty() );
-        TS_ASSERT_EQUALS( (int*)NULL, a.cptr() );
+        TS_ASSERT_EQUALS( (Element*)NULL, a.cptr() );
         TS_ASSERT_EQUALS( 0, a.size() );
+    }
+
+    void testCtorWithCount()
+    {
+        using namespace GN;
+
+        {
+            DynaArray<Element> a(1);
+            TS_ASSERT_EQUALS( 1, a.size() );
+            TS_ASSERT_EQUALS( 1, Element::count );
+        }
+
+        TS_ASSERT_EQUALS( 0, Element::count );
+    }
+
+    void testCtorWithPtrAndCount()
+    {
+        using namespace GN;
+
+        Element::clear();
+
+        Element e[2];
+        TS_ASSERT_EQUALS( 2, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+
+        {
+            DynaArray<Element> a( e, 2 );
+            TS_ASSERT_EQUALS( 2, Element::cctor );
+            TS_ASSERT_EQUALS( 4, Element::count );
+            TS_ASSERT_EQUALS( 0, Element::dtor );
+        }
+
+        TS_ASSERT_EQUALS( 2, Element::count );
+        TS_ASSERT_EQUALS( 2, Element::dtor );
     }
 
     void testCopyCtor()
     {
         using namespace GN;
-        DynaArray<int> a( 1 );
-        DynaArray<int> b( a );
-        DynaArray<int> c;
 
-        TS_ASSERT_EQUALS( 1, b.size() );
-        TS_ASSERT_DIFFERS( a.cptr(), b.cptr() );
+        Element::clear();
 
-        c = b;
-        TS_ASSERT_EQUALS( 1, c.size() );
-        TS_ASSERT_DIFFERS( b.cptr(), c.cptr() );
+        DynaArray<Element> a( 2 );
+
+        TS_ASSERT_EQUALS( 2, Element::count );
+        TS_ASSERT_EQUALS( 2, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+        TS_ASSERT_EQUALS( 0, Element::dtor );
+        TS_ASSERT_EQUALS( 0, Element::cop );
+
+        // 2 cctor for creating new array in b.
+        // 2 dtor for deleteing old array in b.
+        // 2 copy operators for copy c[0,1] to b[0,1]
+        // 1 cctor for copy c[2] to b[2]
+        DynaArray<Element> b( a );
+        TS_ASSERT_EQUALS( 4, Element::count );
+        TS_ASSERT_EQUALS( 2, Element::ctor );
+        TS_ASSERT_EQUALS( 2, Element::cctor );
+        TS_ASSERT_EQUALS( 0, Element::dtor );
+        TS_ASSERT_EQUALS( 0, Element::cop );
+    }
+
+    void testCopyOperator()
+    {
+        using namespace GN;
+
+        Element::clear();
+
+        DynaArray<Element> a( 1 );
+        DynaArray<Element> b( 2 );
+        DynaArray<Element> c( 3 );
+        TS_ASSERT_EQUALS( 6, Element::count );
+        TS_ASSERT_EQUALS( 6, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+        TS_ASSERT_EQUALS( 0, Element::dtor );
+        TS_ASSERT_EQUALS( 0, Element::cop );
+
+        // 2 cctor for creating new array in b.
+        // 2 dtor for deleteing old array in b.
+        // 2 copy operators for copy c[0,1] to b[0,1]
+        // 1 cctor for copy c[2] to b[2]
+        b = c;
+        TS_ASSERT_EQUALS( 7, Element::count );
+        TS_ASSERT_EQUALS( 6, Element::ctor );
+        TS_ASSERT_EQUALS( 3, Element::cctor );
+        TS_ASSERT_EQUALS( 2, Element::dtor );
+        TS_ASSERT_EQUALS( 2, Element::cop );
+
+        // 2 dtor for b[1] and b[2]
+        // 1 copy operator for copy a[0] to b[0]
+        b = a;
+        TS_ASSERT_EQUALS( 5, Element::count );
+        TS_ASSERT_EQUALS( 6, Element::ctor );
+        TS_ASSERT_EQUALS( 3, Element::cctor );
+        TS_ASSERT_EQUALS( 4, Element::dtor );
+        TS_ASSERT_EQUALS( 3, Element::cop );
     }
 
     void testAppend()
@@ -139,6 +366,22 @@ public:
         TS_ASSERT_EQUALS( 2, b.size() );
         TS_ASSERT_EQUALS( 1, b.front() );
         TS_ASSERT_EQUALS( 3, b.back() );
+    }
+
+    void testAppend2()
+    {
+        using namespace GN;
+        DynaArray<Element> a;
+        Element            b;
+        Element            c[2];
+
+        TS_ASSERT_EQUALS( 3, Element::count );
+
+        a.append( b );
+        TS_ASSERT_EQUALS( 4, Element::count );
+
+        a.append( c, 2 );
+        TS_ASSERT_EQUALS( 6, Element::count );
     }
 
     void testBeginEnd()
@@ -230,28 +473,88 @@ public:
         a.popBack(); // should do nothing
     }
 
+    void testEraseAndPopback2()
+    {
+        // make sure erased item is destructed
+
+        using namespace GN;
+
+        Element::clear();
+
+        DynaArray<Element> a( 3 );
+        TS_ASSERT_EQUALS( 3, Element::count );
+        TS_ASSERT_EQUALS( 3, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+        TS_ASSERT_EQUALS( 0, Element::dtor );
+        TS_ASSERT_EQUALS( 0, Element::cop );
+
+        // 2 cop for moving a[1,2] to a[0,1]
+        // 1 dtor for a[2]
+        a.erase( 0 );
+        TS_ASSERT_EQUALS( 2, Element::count );
+        TS_ASSERT_EQUALS( 3, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+        TS_ASSERT_EQUALS( 1, Element::dtor );
+        TS_ASSERT_EQUALS( 2, Element::cop );
+
+        // 1 dtor for a[1]
+        a.popBack();
+        TS_ASSERT_EQUALS( 1, Element::count );
+        TS_ASSERT_EQUALS( 3, Element::ctor );
+        TS_ASSERT_EQUALS( 0, Element::cctor );
+        TS_ASSERT_EQUALS( 2, Element::dtor );
+        TS_ASSERT_EQUALS( 2, Element::cop );
+    }
+
     void testResize()
     {
+        // make sure erased item is destructed
         using namespace GN;
-        DynaArray<int> a;
+        Element::clear();
 
-        a.resize( 2 );
-        TS_ASSERT_EQUALS( 2, a.size() );
+        DynaArray<Element> a( 2 );
+        TS_ASSERT_EQUALS( 2, Element::count );
+
+        a.resize( 3 );
+        TS_ASSERT_EQUALS( 3, Element::count );
 
         a.resize( 4 );
-        TS_ASSERT_EQUALS( 4, a.size() );
+        TS_ASSERT_EQUALS( 4, Element::count );
+
+        a.resize( 1 );
+        TS_ASSERT_EQUALS( 1, Element::count );
     }
 
     void testReserve()
     {
         using namespace GN;
-        DynaArray<int> a;
+        Element::clear();
 
-        a.resize( 2 );
-        TS_ASSERT_EQUALS( 2, a.size() );
+        {
 
-        // reserve should not modify array size
-        a.reserve( 1000 );
-        TS_ASSERT_EQUALS( 2, a.size() );
+            DynaArray<Element> a;
+
+            a.resize( 2 );
+            TS_ASSERT_EQUALS( 2, a.size() );
+            TS_ASSERT_EQUALS( 2, Element::count );
+            TS_ASSERT_EQUALS( 2, Element::ctor );
+
+            // reserve should not modify array size
+            a.reserve( 1000 );
+            TS_ASSERT_EQUALS( 2, a.size() );
+            TS_ASSERT_EQUALS( 2, Element::count );
+            TS_ASSERT_EQUALS( 2, Element::ctor );
+            TS_ASSERT_EQUALS( 2, Element::cctor );
+            TS_ASSERT_EQUALS( 2, Element::dtor );
+        }
+
+        TS_ASSERT_EQUALS( 0, Element::count );
+        TS_ASSERT_EQUALS( 4, Element::dtor );
     }
 };
+
+int DynaArrayTest::Element::count = 0;
+int DynaArrayTest::Element::ctor  = 0;
+int DynaArrayTest::Element::dtor  = 0;
+int DynaArrayTest::Element::cctor = 0;
+int DynaArrayTest::Element::cop = 0;
