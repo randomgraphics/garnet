78c342507df507b7644b7851f2589ac1639fd9bc
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2009-08-25 06:42:42 +0000

    GPU resource database continue...

--- garnet3d/main/src/priv/test/resdb/gpures.h
+++ garnet3d/main/src/priv/test/resdb/gpures.h
@@ -140,7 +140,7 @@ namespace GN { namespace gfx
     };
 
     ///
-    /// comments....
+    /// Uniform resource
     ///
     class UniformResource : public GpuResource
     {
@@ -166,57 +166,286 @@ namespace GN { namespace gfx
         UniformResource( GpuResourceDatabase & db, GpuResourceHandle h ) : GpuResource(db,h) {}
     };
 
-    /*
+    ///
     /// comments....
     ///
-    struct GpuMeshDesc
+    struct MeshResourceDesc
     {
-        VertexFormat  vtxfmt; ///< vertex format
-        VtxBuf     *  vertices[GpuContext::MAX_VERTEX_BUFFERS]; // NULL pointer means vertex data undefined
-        size_t        strides[GpuContext::MAX_VERTEX_BUFFERS];  // vertex buffer strides. 0 means using minimal stride defined by vertex format.
-        size_t        offsets[GpuContext::MAX_VERTEX_BUFFERS];
-        const void *  indices; // Null means index data undefined.
         PrimitiveType prim;   ///< primitive type
         size_t        numvtx; ///< number of vertices
         size_t        numidx; ///< number of indices. 0 means non-indexed mesh
+        VertexFormat  vtxfmt; ///< vertex format
+        const void *  vertices[GpuContext::MAX_VERTEX_BUFFERS]; // NULL pointer means vertex data are undefined
+        size_t        strides[GpuContext::MAX_VERTEX_BUFFERS];  // vertex buffer strides. 0 means using vertex size defined by vertex format.
+        size_t        offsets[GpuContext::MAX_VERTEX_BUFFERS];
+        const void *  indices; // Null means index data are undefined.
     };
 
     ///
     /// comments....
     ///
-    class GpuMeshResource : public GpuResource
+    class MeshResource : public GpuResource
     {
     public:
 
-        void setMesh( const GpuMeshDesc & );
+        /// factory
+        //@{
+        static const Guid & guid();
+        static GpuResourceHandle create( GpuResourceDatabase & db, const char * name, const MeshResourceDesc & desc );
+        static GpuResourceHandle loadFromFile( GpuResourceDatabase & db, const char * filename );
+        //@}
+
+        //@{
+        void setContext( GpuContext * );
+        //@}
 
     protected:
 
         /// protected constructor
-        GpuMeshResource( GpuResourceDatabase & db ) : GpuResource(db,0) {}
+        MeshResource( GpuResourceDatabase & db ) : GpuResource(db,0) {}
+
+    private:
+
+        class Impl;
+        Impl * mImpl;
+    };
+
+    struct EffectResourceDesc
+    {
+        ///
+        /// Shader Prerequisites
+        ///
+        struct ShaderPrerequisites
+        {
+            UInt32 numTextures; ///< minimal number of textures required.
+
+            /// default constructor
+            ShaderPrerequisites()
+                : numTextures(0)
+            {
+            }
+        };
+
+        ///
+        /// Texture descriptor
+        ///
+        struct EffectTextureDesc
+        {
+            /// sampler descriptor
+            SamplerDesc sampler;
+
+            /// default ctor
+            EffectTextureDesc() { sampler.clear(); }
+        };
+
+        ///
+        /// Uniform descriptor
+        ///
+        struct EffectUniformDesc
+        {
+            size_t size;   ///< uniform size
+
+            /// default ctor
+            EffectUniformDesc() : size(0) {}
+        };
+
+        ///
+        /// Effect render target descriptor
+        ///
+        struct EffectRenderTargetDesc
+        {
+            // TODO: minimal size, format and usage requirements, and etc.
+        };
+
+        ///
+        /// Shader descriptor
+        ///
+        struct EffectShaderDesc
+        {
+            GpuProgramDesc      gpd;                ///< GPU Program descriptor
+            std::map<StrA,StrA> textures;           ///< textures. Key is shader parameter name, value is user-visible texture name.
+            std::map<StrA,StrA> uniforms;           ///< uniforms. Key is shader parameter name, value is user-visible uniform name.
+            ShaderPrerequisites prerequisites;      ///< prerequisites of the shader.
+        };
+
+        /// template for single render state
+        template<typename T>
+        struct EffectRenderState
+        {
+            T    value;
+            bool inherited; ///< if true, then this effect will inherit this value from current GPU context.
+
+            /// default ctor
+            EffectRenderState() : inherited(true) {}
+
+            /// set render state value
+            EffectRenderState & operator=( const T & rhs )
+            {
+                value = rhs;
+                inherited = false;
+                return *this;
+            }
+
+            /// set render state value
+            EffectRenderState & operator=( const EffectRenderState<T> & rhs )
+            {
+                value = rhs.value;
+                inherited = rhs.inherited;
+                return *this;
+            }
+        };
+
+        /// render state block
+        struct EffectRenderStateDesc
+        {
+            EffectRenderState<UInt8> fillMode;
+            EffectRenderState<UInt8> cullMode;
+            EffectRenderState<UInt8> frontFace;
+            EffectRenderState<bool>  msaaEnabled;
+            EffectRenderState<bool>  depthTest;
+            EffectRenderState<bool>  depthWrite;
+            EffectRenderState<UInt8> depthFunc;
+
+            // TODO: more render states.
+
+            void clear()
+            {
+                fillMode.inherited = true;
+                cullMode.inherited = true;
+                frontFace.inherited = true;
+                msaaEnabled.inherited = true;
+                depthTest.inherited = true;
+                depthWrite.inherited = true;
+                depthFunc.inherited = true;
+            }
+        };
+
+        ///
+        /// Rendering pass descriptor
+        ///
+        struct EffectPassDesc
+        {
+            StrA                  shader;       ///< Name of shader used in this pass. Can't be empty
+            EffectRenderStateDesc rsd;          ///< pass specific render states
+            DynaArray<StrA>       colortargets; ///< color render targets. Values are user-visible render target names.
+            StrA                  depthstencil; ///< depth render targets. Value is user-visible render target name.
+
+            EffectPassDesc()
+            {
+                colortargets.resize( 1 );
+                colortargets[0] = "color0";
+                depthstencil = "depth";
+            }
+        };
+
+        ///
+        /// Technique descriptor structure
+        ///
+        struct EffectTechniqueDesc
+        {
+            int                       quality; ///< user defined rendering quality. Effect class uses
+                                               ///< the technique with the hightest quality as default technique.
+            DynaArray<EffectPassDesc> passes;  ///< pass list.
+            EffectRenderStateDesc     rsd;     ///< Technique specific render states
+
+            /// default ctor
+            EffectTechniqueDesc() : quality(100) {}
+        };
+
+        std::map<StrA,EffectTextureDesc>      textures;      ///< Texture list
+        std::map<StrA,EffectUniformDesc>      uniforms;      ///< Uniform list
+        std::map<StrA,EffectRenderTargetDesc> rendertargets; ///< Render taret list. Empty means using default setttings: one "color0", one "depth".
+        std::map<StrA,EffectShaderDesc>       shaders;       ///< Shader list
+        std::map<StrA,EffectTechniqueDesc>    techniques;    ///< Technique list. Technique name must be unique.
+        EffectRenderStateDesc                 rsd;           ///< effect specific render states
+
+        ///
+        /// constructor
+        ///
+        EffectResourceDesc()
+        {
+            clear();
+        }
+
+        ///
+        /// Make sure the effect descriptor is valid.
+        ///
+        bool valid() const;
+
+        ///
+        /// clear to a default descriptor
+        ///
+        void clear()
+        {
+            textures.clear();
+            uniforms.clear();
+            rendertargets.clear();
+            shaders.clear();
+            techniques.clear();
+            rsd.clear();
+
+        }
+
+        ///
+        /// setup the descriptor from XML
+        ///
+        bool loadFromXmlNode( const XmlNode & root, const StrA & basedir );
+
+        ///
+        /// write the descriptor to XML
+        ///
+        void saveToXmlNode( const XmlNode & root );
     };
 
     ///
-    /// comments....
+    /// Effect resource
     ///
     class EffectResource : public GpuResource
     {
     public:
 
+        /// return GUID of the effect resource class
+        static const Guid & guid();
+
+        /// create new effect resource. Would fail if the name exists.
+        static GpuResourceHandle create( GpuResourceDatabase & db, const char * name, const EffectResourceDesc & desc );
+
+        /// load effect from file. Would return existing handle, if it is already loaded.
+        static GpuResourceHandle loadFromFile( GpuResourceDatabase & db, const char * filename );
+
+        //@{
+        size_t            getNumPasses() const;
+        void              setContext( size_t pass, GpuContext * rc );
+
+        void              setTexture( const StrA & name, GpuResourceHandle );
+        GpuResourceHandle getTexture( const StrA & name ) const;
+
+        void              setUniform( const StrA & name, GpuResourceHandle );
+        GpuResourceHandle getUniform( const StrA & name ) const;
+
+        void              setRenderTarget( const StrA & name, GpuResourceHandle, size_t face, size_t level, size_t slice );
+        GpuResourceHandle getRenderTarget( const StrA & name, size_t * face = NULL, size_t * level = NULL, size_t * slice = NULL ) const;
+        //@}
+
     protected:
 
         /// protected constructor
-        EffectResource( GpuResourceDatabase & db ) : GpuResource(db,0) {}
-    };*/
+        EffectResource( GpuResourceDatabase & db, GpuResourceHandle h ) : GpuResource(db,h) {}
+
+    private:
+
+        class Impl;
+        Impl * mImpl;
+    };
 
     ///
-    /// comments....
+    /// Model resource descriptor
     ///
     struct ModelResourceDesc
     {
         struct ModelTextureDesc
         {
-            StrA        resourceName; /// if empty, then use the descriptor
+            StrA        resourceName; /// if empty, then create a new texture using the descriptor
             TextureDesc desc;
         };
 
@@ -227,37 +456,18 @@ namespace GN { namespace gfx
             DynaArray<UInt8> defaultValue; ///< if empty, then no default value.
         };
 
-        struct ModelEffectDesc
+        struct SubsetDesc
         {
-            StrA                resourceName;    // effect file name
-            std::map<StrA,StrA> textureBindings; // bind texture (value) to effect parameter (key)
-            std::map<StrA,StrA> uniformBindings; // bind uniform (value) to effect parameter (key)
         };
 
-        struct ModelMeshDesc
-        {
-            StrA        resourceName; ///< if empty, then use the descriptor
-            GpuMeshDesc desc;
-        };
+        StrA                            effectResourceName; // effect resource name. If empty, then create a new effect using effectDesc
+        EffectResourceDesc              effectDesc;
+        std::map<StrA,ModelTextureDesc> textures;           // key is effect parameter name
+        std::map<StrA,ModelUniformDesc> uniforms;           // key is effect parameter name
 
-        struct ModelSubsetDesc
-        {
-            StrA   effect;
-            StrA   mesh;
-
-            // note: (0,0,0,0) means full mesh
-            size_t startvtx;
-            size_t numvtx;
-            size_t startidx;
-            size_t numidx;
-        };
-
-        std::map<StrA,ModelEffectDesc>  effects;
-        std::map<StrA,ModelMeshDesc>    meshes;
-        std::map<StrA,ModelTextureDesc> textures;
-        std::map<StrA,ModelUniformDesc> uniforms;
-
-        DynaArray<ModelSubsetDesc>      subsets;
+        StrA                            meshResourceName; ///< if empty, then create a new mesh using meshDesc
+        MeshResourceDesc                meshResourceDesc;
+        GpuMeshSubset                   subset;
     };
 
     ///
@@ -271,35 +481,19 @@ namespace GN { namespace gfx
     public:
 
         //@{
-
-        /// return GUID of the uniform resource class
-        static const Guid & guid();
-
-        /// create new uniform resource. Would fail if the name exists.
+        static const Guid      & guid();
         static GpuResourceHandle create( GpuResourceDatabase & db, const char * name, const ModelResourceDesc & desc );
-
-        /// load uniform from file. Would return existing handle, if it is already loaded.
         static GpuResourceHandle loadFromFile( GpuResourceDatabase & db, const char * filename );
-
         //@}
 
         //@{
+        void                     setEffect( const StrA & name, GpuResourceHandle effect );
+        GpuResourceHandle        getEffect( const StrA & name ) const;
 
-        void setTexture( const StrA & name, GpuResourceHandle );
-        GpuResourceHandle getTexture( const StrA & name ) const;
+        void                     setMesh( const StrA & name, GpuResourceHandle mesh, const GpuMeshSubset * subset = NULL );
+        GpuResourceHandle        getMesh( const StrA & name, GpuMeshSubset * subset = NULL ) const;
 
-        void setUniform( const StrA & name, GpuResourceHandle );
-        GpuResourceHandle getUniform( const StrA & name ) const;
-
-        void setRenderTarget( const StrA & name, GpuResourceHandle texture, size_t face, size_t leve, size_t slice );
-        GpuResourceHandle getRenderTargetTexture( const StrA & name ) const;
-
-        void setMesh( const StrA & name, GpuResourceHandle mesh );
-        GpuResourceHandle getMesh( const StrA & name ) const;
-
-        void setEffect( const StrA & name, GpuResourceHandle mesh );
-        GpuResourceHandle getEffect( const StrA & name ) const;
-
+        void                     draw() const;
         //@}
 
         // ********************************
@@ -313,7 +507,7 @@ namespace GN { namespace gfx
         //@}
 
         // ********************************
-        // Impl clas
+        // Impl
         // ********************************
 
     private:
