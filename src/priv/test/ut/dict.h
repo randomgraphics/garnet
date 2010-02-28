73fd144d09eff8f98a80b03df1401a54e965ae48
chenlee <chenlirandom@users.noreply.github.com>
2010-02-28 02:29:40 +0000

    implement dictionary template

--- src/priv/test/ut/dict.h
+++ src/priv/test/ut/dict.h
@@ -14,7 +14,7 @@ namespace GN
 
         class Iterator
         {
-            TypelessDict * mDict;
+            UInt8 mBuf[64];
 
         public:
 
@@ -25,6 +25,10 @@ namespace GN
             const void * Key() const;
             const void * Value() const;
             void       * Value();
+            void         CopyFrom( const Iterator & ) const;
+            void         GoToNext() const;
+            bool         Equal( const Iterator & ) const;
+            bool         Less( const Iterator & ) const;
         };
 
         typedef const Iterator ConstIterator;
@@ -35,8 +39,8 @@ namespace GN
             typedef void (*CopyCtorFunc)(void*, const void *);
             typedef void (*DtorFunc)(void*);
             typedef void (*AssignFunc)(void*, const void*);
-            typedef void (*EqualFunc)(const void*, const void*);
-            typedef void (*LessFunc)(const void*, const void*);
+            typedef bool (*EqualFunc)(const void*, const void*);
+            typedef bool (*LessFunc)(const void*, const void*);
 
             size_t       size;
             CtorFunc     ctor;
@@ -52,13 +56,16 @@ namespace GN
 
         ConstIterator   Begin() const;
         Iterator        Begin();
+        void            CopyFrom( const TypelessDict & );
         bool            Empty() const;
         ConstIterator   End() const;
         Iterator        End();
-        VALUE_TYPE *    Find( const KEY_TYPE & key );
-        bool            Insert( const KEY_TYPE & key, const VALUE_TYPE & value );
-        void            RemoveKey( const KEY_TYPE & key );
+        void *          Find( const void * key ) const;
+        void *          FindOrCreate( const void * key );
+        bool            Insert( const void * key, const void * value );
+        void            RemoveKey( const void * key );
         size_t          Size() const;
+
 
     private:
 
@@ -82,11 +89,22 @@ namespace GN
             Iterator() {}
             Iterator( const Iterator & i ) : mTypelessIter(i.mIter) {}
             Iterator( TypelessDict::Iterator & i ) : mTypelessIter(i) {}
-            ~Iterator();
 
             const KEY_TYPE   * Key() const { return (const KEY_TYPE*)mTypelessIter->Key(); }
             const VALUE_TYPE * Value() const { return (const VALUE_TYPE*)mTypelessIter->Value(); }
             VALUE_TYPE       * Value() { return (VALUE_TYPE*)mTypelessIter->Value(); }
+
+            Iterator & operator=( const Iterator & rhs ) { mTypelessIter.CopyFrom( rhs ); return *this; }
+
+            // ++i
+            const Iterator & operator++() const { mTypelessIter.GoToNext(); return *this; }
+
+            // i++
+            friend Iterator operator++( const Iterator & it, int ) { Iterator ret(it); ret.GotoNext(); return ret; }
+
+            bool operator==( const Iterator & rhs ) const { return mTypelessIter.Equal( rhs ); }
+            bool operator!=( const Iterator & rhs ) const { return !mTypelessIter.Equal( rhs ); }
+            bool operator<( const Iterator & rhs ) const { return mTypelessIter.Less( rhs ); }
         };
 
         typedef const Iterator ConstIterator;
@@ -94,28 +112,29 @@ namespace GN
         // public methods
         //@{
 
-        Dictionary() : mTypelessDict( TypeHelper<KEY_TYPE>::sMakeTypeTraits(), TypeHelper<VALUE_TYPE>::sMakeTypeTraits ) {}
+        Dictionary() : mTypelessDict( TypeHelper<KEY_TYPE>::sMakeTypeTraits(), TypeHelper<VALUE_TYPE>::sMakeTypeTraits() ) {}
         Dictionary( const Dictionary & d ) : mTypelessDict( d.mTyplessDict ) {}
         ~Dictionary() {}
 
-        ConstIterator   Begin() const { ConstIterator i( mTypelessDict.Begin() ); return i; }
-        Iterator        Begin() { Iterator i( mTypelessDict.Begin() ); return i; }
-        bool            Empty() const { return mTypelessDict.Empty(); }
-        ConstIterator   End() const { ConstIterator i( mTypelessDict.End() ); return i; }
-        Iterator        End() { Iterator i( mTypelessDict.End() ); return i; }
-        VALUE_TYPE *    Find( const KEY_TYPE & key ) { return (VALUE_TYPE*)mTypelessDict.Find( &key ); }
-        bool            Insert( const KEY_TYPE & key, const VALUE_TYPE & value ) { return mTypelessDict.Insert( &key, &value ); }
-        void            RemoveKey( const KEY_TYPE & key ) { mTypelessDict.RemoveKey( &key ); }
-        size_t          Size() const { return mTypelessDict.Size(); }
+        ConstIterator       Begin() const { ConstIterator i( mTypelessDict.Begin() ); return i; }
+        Iterator            Begin() { Iterator i( mTypelessDict.Begin() ); return i; }
+        bool                Empty() const { return mTypelessDict.Empty(); }
+        ConstIterator       End() const { ConstIterator i( mTypelessDict.End() ); return i; }
+        Iterator            End() { Iterator i( mTypelessDict.End() ); return i; }
+        const VALUE_TYPE *  Find( const KEY_TYPE & key ) const { return (const VALUE_TYPE*)mTypelessDict.Find( &key ); }
+        VALUE_TYPE *        Find( const KEY_TYPE & key ) { return (VALUE_TYPE*)mTypelessDict.Find( &key ); }
+        bool                Insert( const KEY_TYPE & key, const VALUE_TYPE & value ) { return mTypelessDict.Insert( &key, &value ); }
+        void                RemoveKey( const KEY_TYPE & key ) { mTypelessDict.RemoveKey( &key ); }
+        size_t              Size() const { return mTypelessDict.Size(); }
 
         //@}
 
         // operators
         //@{
 
-        Dictionary & operator=( const Dictionary & rhs );
+        Dictionary & operator=( const Dictionary & rhs ) { mTypelessDict.CopyFrom( rhs ); return *this; }
 
-        VALUE_TYPE & operator[]( const KEY_TYPE & key );
+        VALUE_TYPE & operator[]( const KEY_TYPE & key ) { *(VALUE_TYPE*)mTypelessDict.FindOrCreateAt( key ); }
 
         //@}
 
@@ -160,16 +179,16 @@ namespace GN
 
             static void Assign( void * a, const void * b )
             {
-                (*(const T*)a) = (*(const T*)b)
+                (*(T*)a) = (*(const T*)b);
             }
 
-            static void Equal( const void * a, const void * b )
+            static bool Equal( const void * a, const void * b )
             {
                 GN_ASSERT( a && b );
                 return (*(const T*)a) == (*(const T*)b);
             }
 
-            static void Less( const void * a, const void * b )
+            static bool Less( const void * a, const void * b )
             {
                 GN_ASSERT( a && b );
                 return (*(const T*)a) < (*(const T*)b);
