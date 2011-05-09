6535355fc6fbdbc1948d3b865bc7f91d0b917466
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2011-05-09 21:37:45 +0000

    source folder reorg

--- garnet3d/main/src/priv/core/all_public_headers.cpp
+++
@@ -1,8 +0,0 @@
-#define GN_CORE_INTERNAL
-#include <garnet/GNbase.h>
-#include <garnet/GNd3d.h>
-#include <garnet/GNengine.h>
-#include <garnet/GNgfx.h>
-#include <garnet/GNinput.h>
-#include <garnet/GNutil.h>
-#include <garnet/GNwin.h>
