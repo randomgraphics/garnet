2718818c5c1f2bacb04f3af9e147405350e6a6a8
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2012-09-28 05:18:00 +0000

    d3d11hook working in progress.

--- garnet3d/main/src/priv/tool/d3d11hook/dll/ID3D11Debug.h
+++ garnet3d/main/src/priv/tool/d3d11hook/dll/ID3D11Debug.h
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetFeatureMask(
     UINT Mask);
-NullPtr<void (D3D11DebugHook::*)(UINT)> _SetFeatureMask_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(UINT &)> _SetFeatureMask_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, UINT)> _SetFeatureMask_post_ptr;
 // -----------------------------------------------------------------------------
 virtual UINT STDMETHODCALLTYPE GetFeatureMask();
@@ -10,7 +10,7 @@ NullPtr<void (D3D11DebugHook::*)(UINT)> _GetFeatureMask_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetPresentPerRenderOpDelay(
     UINT Milliseconds);
-NullPtr<void (D3D11DebugHook::*)(UINT)> _SetPresentPerRenderOpDelay_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(UINT &)> _SetPresentPerRenderOpDelay_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, UINT)> _SetPresentPerRenderOpDelay_post_ptr;
 // -----------------------------------------------------------------------------
 virtual UINT STDMETHODCALLTYPE GetPresentPerRenderOpDelay();
@@ -19,25 +19,25 @@ NullPtr<void (D3D11DebugHook::*)(UINT)> _GetPresentPerRenderOpDelay_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetSwapChain(
     IDXGISwapChain * pSwapChain);
-NullPtr<void (D3D11DebugHook::*)(IDXGISwapChain *)> _SetSwapChain_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(IDXGISwapChain * &)> _SetSwapChain_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, IDXGISwapChain *)> _SetSwapChain_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE GetSwapChain(
     IDXGISwapChain ** ppSwapChain);
-NullPtr<void (D3D11DebugHook::*)(IDXGISwapChain **)> _GetSwapChain_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(IDXGISwapChain ** &)> _GetSwapChain_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, IDXGISwapChain **)> _GetSwapChain_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE ValidateContext(
     ID3D11DeviceContext * pContext);
-NullPtr<void (D3D11DebugHook::*)(ID3D11DeviceContext *)> _ValidateContext_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(ID3D11DeviceContext * &)> _ValidateContext_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, ID3D11DeviceContext *)> _ValidateContext_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE ReportLiveDeviceObjects(
     D3D11_RLDO_FLAGS Flags);
-NullPtr<void (D3D11DebugHook::*)(D3D11_RLDO_FLAGS)> _ReportLiveDeviceObjects_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(D3D11_RLDO_FLAGS &)> _ReportLiveDeviceObjects_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, D3D11_RLDO_FLAGS)> _ReportLiveDeviceObjects_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE ValidateContextForDispatch(
     ID3D11DeviceContext * pContext);
-NullPtr<void (D3D11DebugHook::*)(ID3D11DeviceContext *)> _ValidateContextForDispatch_pre_ptr;
+NullPtr<void (D3D11DebugHook::*)(ID3D11DeviceContext * &)> _ValidateContextForDispatch_pre_ptr;
 NullPtr<void (D3D11DebugHook::*)(HRESULT, ID3D11DeviceContext *)> _ValidateContextForDispatch_post_ptr;
--- garnet3d/main/src/priv/tool/d3d11hook/dll/ID3D11InfoQueue.h
+++ garnet3d/main/src/priv/tool/d3d11hook/dll/ID3D11InfoQueue.h
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetMessageCountLimit(
     UINT64 MessageCountLimit);
-NullPtr<void (D3D11InfoQueueHook::*)(UINT64)> _SetMessageCountLimit_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(UINT64 &)> _SetMessageCountLimit_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, UINT64)> _SetMessageCountLimit_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE ClearStoredMessages();
@@ -12,7 +12,7 @@ virtual HRESULT STDMETHODCALLTYPE GetMessage(
     UINT64 MessageIndex,
     D3D11_MESSAGE * pMessage,
     SIZE_T * pMessageByteLength);
-NullPtr<void (D3D11InfoQueueHook::*)(UINT64, D3D11_MESSAGE *, SIZE_T *)> _GetMessage_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(UINT64 &, D3D11_MESSAGE * &, SIZE_T * &)> _GetMessage_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, UINT64, D3D11_MESSAGE *, SIZE_T *)> _GetMessage_post_ptr;
 // -----------------------------------------------------------------------------
 virtual UINT64 STDMETHODCALLTYPE GetNumMessagesAllowedByStorageFilter();
@@ -41,13 +41,13 @@ NullPtr<void (D3D11InfoQueueHook::*)(UINT64)> _GetMessageCountLimit_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE AddStorageFilterEntries(
     D3D11_INFO_QUEUE_FILTER * pFilter);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *)> _AddStorageFilterEntries_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &)> _AddStorageFilterEntries_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *)> _AddStorageFilterEntries_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE GetStorageFilter(
     D3D11_INFO_QUEUE_FILTER * pFilter,
     SIZE_T * pFilterByteLength);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *, SIZE_T *)> _GetStorageFilter_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &, SIZE_T * &)> _GetStorageFilter_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *, SIZE_T *)> _GetStorageFilter_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE ClearStorageFilter();
@@ -64,7 +64,7 @@ NullPtr<void (D3D11InfoQueueHook::*)(HRESULT)> _PushCopyOfStorageFilter_post_ptr
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE PushStorageFilter(
     D3D11_INFO_QUEUE_FILTER * pFilter);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *)> _PushStorageFilter_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &)> _PushStorageFilter_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *)> _PushStorageFilter_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE PopStorageFilter();
@@ -77,13 +77,13 @@ NullPtr<void (D3D11InfoQueueHook::*)(UINT)> _GetStorageFilterStackSize_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE AddRetrievalFilterEntries(
     D3D11_INFO_QUEUE_FILTER * pFilter);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *)> _AddRetrievalFilterEntries_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &)> _AddRetrievalFilterEntries_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *)> _AddRetrievalFilterEntries_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE GetRetrievalFilter(
     D3D11_INFO_QUEUE_FILTER * pFilter,
     SIZE_T * pFilterByteLength);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *, SIZE_T *)> _GetRetrievalFilter_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &, SIZE_T * &)> _GetRetrievalFilter_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *, SIZE_T *)> _GetRetrievalFilter_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE ClearRetrievalFilter();
@@ -100,7 +100,7 @@ NullPtr<void (D3D11InfoQueueHook::*)(HRESULT)> _PushCopyOfRetrievalFilter_post_p
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE PushRetrievalFilter(
     D3D11_INFO_QUEUE_FILTER * pFilter);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER *)> _PushRetrievalFilter_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_INFO_QUEUE_FILTER * &)> _PushRetrievalFilter_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_INFO_QUEUE_FILTER *)> _PushRetrievalFilter_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE PopRetrievalFilter();
@@ -116,51 +116,51 @@ virtual HRESULT STDMETHODCALLTYPE AddMessage(
     D3D11_MESSAGE_SEVERITY Severity,
     D3D11_MESSAGE_ID ID,
     LPCSTR pDescription);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY, D3D11_MESSAGE_SEVERITY, D3D11_MESSAGE_ID, LPCSTR)> _AddMessage_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY &, D3D11_MESSAGE_SEVERITY &, D3D11_MESSAGE_ID &, LPCSTR &)> _AddMessage_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_MESSAGE_CATEGORY, D3D11_MESSAGE_SEVERITY, D3D11_MESSAGE_ID, LPCSTR)> _AddMessage_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE AddApplicationMessage(
     D3D11_MESSAGE_SEVERITY Severity,
     LPCSTR pDescription);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY, LPCSTR)> _AddApplicationMessage_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY &, LPCSTR &)> _AddApplicationMessage_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_MESSAGE_SEVERITY, LPCSTR)> _AddApplicationMessage_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetBreakOnCategory(
     D3D11_MESSAGE_CATEGORY Category,
     BOOL bEnable);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY, BOOL)> _SetBreakOnCategory_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY &, BOOL &)> _SetBreakOnCategory_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_MESSAGE_CATEGORY, BOOL)> _SetBreakOnCategory_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetBreakOnSeverity(
     D3D11_MESSAGE_SEVERITY Severity,
     BOOL bEnable);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY, BOOL)> _SetBreakOnSeverity_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY &, BOOL &)> _SetBreakOnSeverity_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_MESSAGE_SEVERITY, BOOL)> _SetBreakOnSeverity_post_ptr;
 // -----------------------------------------------------------------------------
 virtual HRESULT STDMETHODCALLTYPE SetBreakOnID(
     D3D11_MESSAGE_ID ID,
     BOOL bEnable);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_ID, BOOL)> _SetBreakOnID_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_ID &, BOOL &)> _SetBreakOnID_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(HRESULT, D3D11_MESSAGE_ID, BOOL)> _SetBreakOnID_post_ptr;
 // -----------------------------------------------------------------------------
 virtual BOOL STDMETHODCALLTYPE GetBreakOnCategory(
     D3D11_MESSAGE_CATEGORY Category);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY)> _GetBreakOnCategory_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_CATEGORY &)> _GetBreakOnCategory_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(BOOL, D3D11_MESSAGE_CATEGORY)> _GetBreakOnCategory_post_ptr;
 // -----------------------------------------------------------------------------
 virtual BOOL STDMETHODCALLTYPE GetBreakOnSeverity(
     D3D11_MESSAGE_SEVERITY Severity);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY)> _GetBreakOnSeverity_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_SEVERITY &)> _GetBreakOnSeverity_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(BOOL, D3D11_MESSAGE_SEVERITY)> _GetBreakOnSeverity_post_ptr;
 // -----------------------------------------------------------------------------
 virtual BOOL STDMETHODCALLTYPE GetBreakOnID(
     D3D11_MESSAGE_ID ID);
-NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_ID)> _GetBreakOnID_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(D3D11_MESSAGE_ID &)> _GetBreakOnID_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(BOOL, D3D11_MESSAGE_ID)> _GetBreakOnID_post_ptr;
 // -----------------------------------------------------------------------------
 virtual void STDMETHODCALLTYPE SetMuteDebugOutput(
     BOOL bMute);
-NullPtr<void (D3D11InfoQueueHook::*)(BOOL)> _SetMuteDebugOutput_pre_ptr;
+NullPtr<void (D3D11InfoQueueHook::*)(BOOL &)> _SetMuteDebugOutput_pre_ptr;
 NullPtr<void (D3D11InfoQueueHook::*)(BOOL)> _SetMuteDebugOutput_post_ptr;
 // -----------------------------------------------------------------------------
 virtual BOOL STDMETHODCALLTYPE GetMuteDebugOutput();
