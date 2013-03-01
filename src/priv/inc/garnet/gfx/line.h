8c57e6063fd28fa55b87ca9d3e777d88b6837afc
chenlee <chenlirandom@users.noreply.github.com>
2013-03-01 01:01:44 +0000

    implement general thick line renderer (not tested yet)

--- src/priv/inc/garnet/gfx/line.h
+++ src/priv/inc/garnet/gfx/line.h
@@ -128,6 +128,119 @@ namespace GN { namespace gfx
         ///
         void flush();
     };
+
+    ///
+    /// think line renderer
+    ///
+    class ThickLineRenderer : public StdClass
+    {
+        GN_DECLARE_STDCLASS( ThickLineRenderer, StdClass );
+
+        // ********************************
+        // ctor/dtor
+        // ********************************
+
+        //@{
+    public:
+        ThickLineRenderer();
+        virtual ~ThickLineRenderer() { quit(); }
+        //@}
+
+        // ********************************
+        // from StdClass
+        // ********************************
+
+        //@{
+    public:
+        bool init( Gpu & );
+        void quit();
+    private:
+        void clear() { m_Drawing = false; }
+        //@}
+
+        // ********************************
+        // public functions
+        // ********************************
+    public:
+
+        struct ThickLineVertex
+        {
+            float    x, y, z;
+            float    u, v;
+            uint32   color;
+        };
+
+        struct ThickLineParameters
+        {
+            Matrix44f worldview;          // matrix that transform vertex from object space to view space
+            Matrix44f proj;               // matrix that transform vertex from view space to clip space
+            float     width;              // line width
+            bool      widthInScreenSpace; // true  : line width is in pixels in screen space
+                                          // false : line width is in view space
+        };
+
+        bool drawBegin( const ThickLineParameters & parameters );
+        void drawEnd();
+
+        void line( const ThickLineVertex & v0, const ThickLineVertex & v1 );
+        void line( float x1, float y1, float z1, float x2, float y2, float z2, uint32 abgr );
+        void lineList( const ThickLineVertex * vertices, size_t numverts );
+
+        // ********************************
+        // private variables
+        // ********************************
+    private:
+
+        struct PrivateParameters : public ThickLineParameters
+        {
+            Matrix44f wvp; // world * view * proj
+            float screenWidth, screenHeight; // screen size in pixels
+            float endPointHalfWidth, endPointHalfHeight; // size of line end point in clip space.
+        };
+
+        struct PrivateVertex
+        {
+            Vector4f position; // position in clip space
+            uint32   color;
+            float    u, v;
+            uint32   _; // padding
+        };
+
+        struct EndPoint
+        {
+            float posl, posr, post, posb, posz, posw;
+            float texl, texr, text, texb;
+            uint32 color;
+
+            PrivateVertex * topLeft( PrivateVertex * v );
+            PrivateVertex * topRight( PrivateVertex * v );
+            PrivateVertex * bottomLeft( PrivateVertex * v );
+            PrivateVertex * bottomRight( PrivateVertex * v );
+        };
+
+        Gpu        * mGpu;
+        GpuContext   mContext;
+
+        bool m_Drawing;
+        PrivateParameters m_Parameters;
+
+        static const size_t MAX_VERTICES = 1024;
+        PrivateVertex m_Vertices[MAX_VERTICES];
+        uint32        m_NumVertices;
+
+        // ********************************
+        // private functions
+        // ********************************
+    private:
+
+        void calcEndPoint(
+            EndPoint              & endpoint,
+            const ThickLineVertex & vertex );
+
+        PrivateVertex * newPolygon6();
+
+        void flush();
+    };
 }}
 
 // *****************************************************************************
