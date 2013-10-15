3135bf6a0a8f5be46b8c6852edecb7265f121e6f
chenlee <chenlirandom@users.noreply.github.com>
2013-10-15 01:02:27 +0000

    temporary check point (build is broken)

---
+++ src/priv/tool/d3d11hook/dll/IDXGIFactory1.h
@@ -0,0 +1,11 @@
+// script generated file. DO NOT edit.
+// -----------------------------------------------------------------------------
+virtual HRESULT STDMETHODCALLTYPE EnumAdapters1(
+    /* [in] */ UINT Adapter,
+    _Out_  IDXGIAdapter1 ** ppAdapter);
+NullPtr<void (DXGIFactory1Hook::*)(/* [in] */ UINT &, _Out_  IDXGIAdapter1 ** &)> _EnumAdapters1_pre_ptr;
+NullPtr<void (DXGIFactory1Hook::*)(HRESULT, /* [in] */ UINT, _Out_  IDXGIAdapter1 **)> _EnumAdapters1_post_ptr;
+// -----------------------------------------------------------------------------
+virtual BOOL STDMETHODCALLTYPE IsCurrent();
+NullPtr<void (DXGIFactory1Hook::*)()> _IsCurrent_pre_ptr;
+NullPtr<void (DXGIFactory1Hook::*)(BOOL)> _IsCurrent_post_ptr;
--- src/priv/tool/d3d11hook/dll/d3d11hook.h
+++ src/priv/tool/d3d11hook/dll/d3d11hook.h
@@ -17,23 +17,15 @@
 // D3D11 interfaces
 // *****************************************************************************
 
-class D3D11DebugHook;
-class D3D11InfoQueueHook;
-
-class D3D11DeviceHook : public UnknownHook<D3D11DeviceHook, ID3D11Device, CID_ID3D11Device_COUNT>
+class D3D11DeviceHook : public HookBase<D3D11DeviceHook, ID3D11Device>
 {
 public:
 
     D3D11DeviceHook(ID3D11Device * realobj)
-        : IUNKNOWN_BASE_TYPE(realobj)
+        : BASE_CLASS(realobj)
     {
-        // Note: this will add reverse QI to other hook classes too.
-        AddQIInterface<DXGIAdapterHook>();
-        AddQIInterface<D3D11DebugHook>();
-        AddQIInterface<D3D11InfoQueueHook>();
-
         // setup special pre/post function pointers
-        _preFuncPtr[CID_ID3D11Device_CreateBuffer] = (METHOD_FUNC_PTR)&D3D11DeviceHook::CustomCreateBuffer_PRE;
+        _CreateBuffer_pre_ptr = (METHOD_FUNC_PTR)&D3D11DeviceHook::CustomCreateBuffer_PRE;
     }
 
     ~D3D11DeviceHook()
@@ -42,19 +34,81 @@ public:
 
     void STDMETHODCALLTYPE CustomCreateBuffer_PRE(
         const D3D11_BUFFER_DESC * &,
-        const D3D11_SUBRESOURCE_DATA * &);
+        const D3D11_SUBRESOURCE_DATA * &,
+        ID3D11Buffer ** &);
 
 public:
 
     #include "ID3D11Device.h"
 };
 
-class D3D11DeviceContextHook : public D3D11DeviceChildHook<D3D11DeviceContextHook, ID3D11DeviceContext, CID_ID3D11DeviceContext_COUNT>
+class D3D11DeviceChildHook : public HookBase<D3D11DeviceChildHook, ID3D11DeviceChild>
+{
+
+protected:
+
+    D3D11DeviceChildHook(REAL_CLASS * realobj) : BASE_CLASS(realobj)
+    {
+    }
+
+    virtual ~D3D11DeviceChildHook()
+    {
+    }
+
+public:
+
+    virtual void STDMETHODCALLTYPE GetDevice(
+        /* [annotation] */
+        __out  ID3D11Device **ppDevice)
+    {
+        if (nullptr == ppDevice) return;
+
+        GN::AutoComPtr<ID3D11Device> pRealDevice;
+        GetRealObj()->GetDevice(&pRealDevice);
+
+        ID3D11Device * pHookedDevice = GetHookedInstanceFromReal<ID3D11Device, ID3D11Device>(pRealDevice);
+        GN_ASSERT(nullptr != pHookedDevice);
+
+        *ppDevice = pHookedDevice;
+    }
+
+    virtual HRESULT STDMETHODCALLTYPE GetPrivateData(
+        /* [annotation] */
+        __in  REFGUID guid,
+        /* [annotation] */
+        __inout  UINT *pDataSize,
+        /* [annotation] */
+        __out_bcount_opt( *pDataSize )  void *pData)
+    {
+        return GetRealObj()->GetPrivateData(guid, pDataSize, pData);
+    }
+
+    virtual HRESULT STDMETHODCALLTYPE SetPrivateData(
+        /* [annotation] */
+        __in  REFGUID guid,
+        /* [annotation] */
+        __in  UINT DataSize,
+        /* [annotation] */
+        __in_bcount_opt( DataSize )  const void *pData)
+    {
+        return GetRealObj()->SetPrivateData(guid, DataSize, pData);
+    }
+
+    virtual HRESULT STDMETHODCALLTYPE SetPrivateDataInterface(
+        /* [annotation] */
+        __in  REFGUID guid,
+        /* [annotation] */
+        __in_opt  const IUnknown *pData)
+    {
+        return GetRealObj()->SetPrivateDataInterface(guid, pData);
+    }
+};
+
+class D3D11DeviceContextHook : public HookBase<D3D11DeviceContextHook, ID3D11DeviceContext>
 {
 public:
 
-    D3D11DeviceContextHook(ID3D11DeviceContext * realobj)
-        : ID3D11DEVICECHILD_BASE_TYPE(realobj)
+    D3D11DeviceContextHook(ID3D11DeviceContext * realobj): BASE_CLASS(realobj)
     {
     }
 
@@ -65,12 +119,11 @@ public:
     #include "ID3D11DeviceContext.h"
 };
 
-class D3D11DebugHook : public UnknownHook<D3D11DebugHook, ID3D11Debug, CID_ID3D11DeviceContext_COUNT>
+class D3D11DebugHook : public HookBase<D3D11DebugHook, ID3D11Debug>
 {
 public:
 
-    D3D11DebugHook(ID3D11Debug * realobj)
-        : IUNKNOWN_BASE_TYPE(realobj)
+    D3D11DebugHook(ID3D11Debug * realobj) : BASE_CLASS(realobj)
     {
     }
 
@@ -81,12 +134,11 @@ public:
     #include "ID3D11Debug.h"
 };
 
-class D3D11InfoQueueHook : public UnknownHook<D3D11InfoQueueHook, ID3D11InfoQueue, CID_ID3D11DeviceContext_COUNT>
+class D3D11InfoQueueHook : public HookBase<D3D11InfoQueueHook, ID3D11InfoQueue>
 {
 public:
 
-    D3D11InfoQueueHook(ID3D11InfoQueue * realobj)
-        : IUNKNOWN_BASE_TYPE(realobj)
+    D3D11InfoQueueHook(ID3D11InfoQueue * realobj) : BASE_CLASS(realobj)
     {
     }
 
