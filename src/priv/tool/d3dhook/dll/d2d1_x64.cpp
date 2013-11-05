fae1945d51568fd1abfac794fe853ad290cc89c7
chenlee <chenlirandom@users.noreply.github.com>
2013-11-05 18:45:05 +0000

    d3dhook: remove assembly implementation of d2d1 hook function

--- src/priv/tool/d3dhook/dll/d2d1_x64.cpp
+++ src/priv/tool/d3dhook/dll/d2d1_x64.cpp
@@ -1,16 +1,17 @@
 #include "pch.h"
 #include "hooks_exports.h"
-HINSTANCE mHinst = 0, mHinstDLL = 0;
-extern "C" UINT_PTR mProcs[11] = {0};
 
-LPCSTR mImportNames[] = {"D2D1ConvertColorSpace", "D2D1CreateDevice", "D2D1CreateDeviceContext", "D2D1CreateFactory", "D2D1InvertMatrix", "D2D1IsMatrixInvertible", "D2D1MakeRotateMatrix", "D2D1MakeSkewMatrix", "D2D1SinCos", "D2D1Tan", "D2D1Vec3Length"};
+HINSTANCE mHinst = 0, mHinstDLL = 0;
+extern "C" UINT_PTR mProcs[12] = {0};
+
+LPCSTR mImportNames[] = {"D2D1ComputeMaximumScaleFactor", "D2D1ConvertColorSpace", "D2D1CreateDevice", "D2D1CreateDeviceContext", "D2D1CreateFactory", "D2D1InvertMatrix", "D2D1IsMatrixInvertible", "D2D1MakeRotateMatrix", "D2D1MakeSkewMatrix", "D2D1SinCos", "D2D1Tan", "D2D1Vec3Length"};
 BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID ) {
 	mHinst = hinstDLL;
 	if ( fdwReason == DLL_PROCESS_ATTACH ) {
-		mHinstDLL = LoadLibraryW( GetRealDllPath(L"d2d1.dll") );
+		mHinstDLL = LoadLibrary( GetRealDllPath(L"d2d1.dll") );
 		if ( !mHinstDLL )
 			return ( FALSE );
-		for ( int i = 0; i < 11; i++ )
+		for ( int i = 0; i < 12; i++ )
 			mProcs[ i ] = (UINT_PTR)GetProcAddress( mHinstDLL, mImportNames[ i ] );
 	} else if ( fdwReason == DLL_PROCESS_DETACH ) {
 		FreeLibrary( mHinstDLL );
@@ -18,6 +19,7 @@ BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID ) {
 	return ( TRUE );
 }
 
+extern "C" void D2D1ComputeMaximumScaleFactor_wrapper();
 extern "C" void D2D1ConvertColorSpace_wrapper();
 #if 0
 extern "C" void D2D1CreateDevice_wrapper();
--- src/priv/tool/d3dhook/dll/d2d1_x64_asm.asm
+++ src/priv/tool/d3dhook/dll/d2d1_x64_asm.asm
@@ -1,36 +1,39 @@
 .code
 extern mProcs:QWORD
-D2D1ConvertColorSpace_wrapper proc
+D2D1ComputeMaximumScaleFactor_wrapper proc
 	jmp mProcs[0*8]
-D2D1ConvertColorSpace_wrapper endp
-D2D1CreateDevice_wrapper proc
+D2D1ComputeMaximumScaleFactor_wrapper endp
+D2D1ConvertColorSpace_wrapper proc
 	jmp mProcs[1*8]
-D2D1CreateDevice_wrapper endp
-D2D1CreateDeviceContext_wrapper proc
-	jmp mProcs[2*8]
-D2D1CreateDeviceContext_wrapper endp
-D2D1CreateFactory_wrapper proc
-	jmp mProcs[3*8]
-D2D1CreateFactory_wrapper endp
+D2D1ConvertColorSpace_wrapper endp
+;;D2D1CreateDevice_wrapper proc
+;;	jmp mProcs[2*8]
+;;D2D1CreateDevice_wrapper endp
+;;D2D1CreateDeviceContext_wrapper proc
+;;	jmp mProcs[3*8]
+;;D2D1CreateDeviceContext_wrapper endp
+;;D2D1CreateFactory_wrapper proc
+;;	jmp mProcs[4*8]
+;;D2D1CreateFactory_wrapper endp
 D2D1InvertMatrix_wrapper proc
-	jmp mProcs[4*8]
+	jmp mProcs[5*8]
 D2D1InvertMatrix_wrapper endp
 D2D1IsMatrixInvertible_wrapper proc
-	jmp mProcs[5*8]
+	jmp mProcs[6*8]
 D2D1IsMatrixInvertible_wrapper endp
 D2D1MakeRotateMatrix_wrapper proc
-	jmp mProcs[6*8]
+	jmp mProcs[7*8]
 D2D1MakeRotateMatrix_wrapper endp
 D2D1MakeSkewMatrix_wrapper proc
-	jmp mProcs[7*8]
+	jmp mProcs[8*8]
 D2D1MakeSkewMatrix_wrapper endp
 D2D1SinCos_wrapper proc
-	jmp mProcs[8*8]
+	jmp mProcs[9*8]
 D2D1SinCos_wrapper endp
 D2D1Tan_wrapper proc
-	jmp mProcs[9*8]
+	jmp mProcs[10*8]
 D2D1Tan_wrapper endp
 D2D1Vec3Length_wrapper proc
-	jmp mProcs[10*8]
+	jmp mProcs[11*8]
 D2D1Vec3Length_wrapper endp
 end
