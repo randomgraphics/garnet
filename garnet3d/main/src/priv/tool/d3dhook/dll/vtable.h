6f59c2036bc7b31e216e77e671e5c3f3487cefd1
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2013-11-09 01:44:12 +0000

    d3dhook: vtable hook working in progress

--- garnet3d/main/src/priv/tool/d3dhook/dll/vtable.h
+++ garnet3d/main/src/priv/tool/d3dhook/dll/vtable.h
@@ -7,3 +7,53 @@
 #define HOOK_WARN_LOG(...)  GN_WARN(GN::getLogger("GN.d3dhook"))(__VA_ARGS__)
 #define HOOK_ASSERT(x)      GN_ASSERT(x)
 #define HOOK_RIP()          GN_UNEXPECTED()
+
+// -----------------------------------------------------------------------------
+// common call trace utilities
+// -----------------------------------------------------------------------------
+
+namespace calltrace
+{
+    /// Global switch to turn on/off call tracking
+    extern bool g_callTraceEnabled;
+
+    /// return call level (including current call)
+    int enter(const wchar_t * text);
+
+    /// return call level (including current call)
+    int enter(const char * text);
+
+    void leave();
+
+    /// auotmatic call trace helper
+    class AutoTrace
+    {
+        bool _enabled;
+
+    public:
+
+        AutoTrace(const wchar_t * text) : _enabled(g_callTraceEnabled)
+        {
+            if (_enabled)
+            {
+                enter(text);
+            }
+        }
+
+        AutoTrace(const char * text) : _enabled(g_callTraceEnabled)
+        {
+            if (_enabled)
+            {
+                enter(text);
+            }
+        }
+
+        ~AutoTrace()
+        {
+            if (_enabled)
+            {
+                leave();
+            }
+        }
+    };
+};
