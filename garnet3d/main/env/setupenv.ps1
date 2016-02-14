0ef4d54ba1835d54a7d198540a339bd27d1dfe66
chenlee <chenlee@f06bb278-bfd0-6e4d-a108-ac6744eb693e>
2016-02-14 19:49:00 +0000

    upgrade scons to 2.4.1

--- garnet3d/main/env/setupenv.ps1
+++ garnet3d/main/env/setupenv.ps1
@@ -476,7 +476,7 @@ if( "xenon" -eq $env:GN_BUILD_COMPILER )
 }
 else
 {
-    $SCONS_DIR= "$GARNET_ROOT\env\scons\2.2.0"
+    $SCONS_DIR= "$GARNET_ROOT\env\scons\2.4.1"
     $env:SCONS_LIB_DIR    = "$SCONS_DIR\engine"
     $env:SCONS_SCRIPT_DIR = "$SCONS_DIR\script\"
 }
