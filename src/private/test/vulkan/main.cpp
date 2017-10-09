2c16a5c489dd8b399c55e1a3e9b3c8774af34f00
Chen Li <chenlirandom@gmail.com>
2017-10-08 22:53:04 -0700

    intial checkin of Vulkan test (working in progress)

---
+++ src/private/test/vulkan/main.cpp
@@ -0,0 +1,125 @@
+#include "pch.h"
+#include <vector>
+
+using namespace GN;
+
+static GN::Logger * sLogger = GN::getLogger("GN.test.vulkan");
+
+#define CHECK_VK(x) if (VK_SUCCESS == (x)) return;
+
+struct App {
+    bool          success;
+    win::Window * win;
+    VkInstance    vkInst;
+    VkDebugReportCallbackEXT vkDebugReport;
+    VkDevice      vkDev;
+
+    App()
+        : success(false)
+        , win(nullptr)
+        , vkInst(nullptr)
+        , vkDebugReport(nullptr)
+        , vkDev(nullptr)
+    {
+        win = win::createWindow(win::WindowCreationParams{
+            "vulkan test",
+            0, // parent
+            1960, 1080,
+            true, true, false, true
+        });
+
+        // query layers
+        uint32 layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+        StrA s = "available Vulkan layers:\n";
+        for (const auto & l : availableLayers) {
+            s += str::format("\t%s\n", l.layerName);
+        }
+        GN_INFO(sLogger)(s);
+    
+        // query extensions
+        uint32 extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableExtensions.data());
+        s = "available Vulkan extensions:\n";
+        for (const auto & e : availableExtensions) {
+            s += str::format("\t%s\n", e.extensionName);
+        }
+        GN_INFO(sLogger)(s);
+        
+        // create VK instance
+        std::vector<const char*> layers = {
+            "VK_LAYER_LUNARG_standard_validation"
+        };
+        std::vector<const char *> extensions = {
+            VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+        };
+        auto ci = VkInstanceCreateInfo {
+            VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            nullptr,
+            0, // flags,
+            nullptr,
+            (uint32)layers.size(), layers.data(),
+            (uint32)extensions.size(), extensions.data()
+        };
+        CHECK_VK(vkCreateInstance(&ci, nullptr, &vkInst));
+
+        // setup debug callback
+        VkDebugReportCallbackCreateInfoEXT createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+        createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+        createInfo.pfnCallback = debugCallback;
+        CHECK_VK(CreateDebugReportCallbackEXT(vkInst, &createInfo, nullptr, &vkDebugReport));
+
+        // done
+        success = true;
+    }
+
+    ~App() {
+        if (vkDebugReport) DestroyDebugReportCallbackEXT(vkInst, vkDebugReport, nullptr);
+        if (vkInst) vkDestroyInstance(vkInst, nullptr);
+        if (win) delete win;
+    }
+
+    VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback) {
+        auto func = (PFN_vkCreateDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+        if (func != nullptr) {
+            return func(instance, pCreateInfo, pAllocator, pCallback);
+        } else {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+    }
+
+    void DestroyDebugReportCallbackEXT(VkInstance instance, VkDebugReportCallbackEXT callback, const VkAllocationCallbacks* pAllocator) {
+        auto func = (PFN_vkDestroyDebugReportCallbackEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT");
+        if (func != nullptr) {
+            func(instance, callback, pAllocator);
+        }
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugReportFlagsEXT,// flags,
+        VkDebugReportObjectTypeEXT,// objType,
+        uint64_t,// obj,
+        size_t,// location,
+        int32_t,// code,
+        const char*,// layerPrefix,
+        const char* msg,
+        void* userData) {
+        GN_UNUSED_PARAM(userData);
+        GN_INFO(sLogger)("[vulkan] %s", msg);
+        return VK_FALSE;
+    }
+
+    int run() {
+        return 0;
+    }
+};
+
+int main() {
+    App app;
+    return app.success ? app.run() : -1;
+}
