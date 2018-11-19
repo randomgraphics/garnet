02a6abdb085296257b596c99d0eb705e4ef60a2a
Chen Li <rgba@fb.com>
2018-11-19 11:14:00 -0800

    added d3d12 test app (working in progress)

--- env/cmake/FindDirectX.cmake
+++ env/cmake/FindDirectX.cmake
@@ -4,6 +4,7 @@
 # D3D9_FOUND
 # D3D11_FOUND
 # D3D11_1_FOUND
+# D3D12_FOUND
 # DIRECTX_LIBRARIES
 
 set(D3D9_FOUND)
@@ -25,6 +26,11 @@ if(WIN32)
     check_include_file(d3d11_1.h D3D11_1_FOUND)
     if (D3D11_FOUND)
         list(APPEND DIRECTX_LIBRARIES dxgi.lib dxguid.lib d3d11.lib dwrite.lib d2d1.lib d3dcompiler.lib xinput.lib)
+    endif()
+
+    check_include_file(d3d12.h D3D12_FOUND)
+    if (D3D12_FOUND)
+        list(APPEND DIRECTX_LIBRARIES dxgi.lib dxguid.lib d3d12.lib dwrite.lib d2d1.lib d3dcompiler.lib xinput.lib)
     endif()
 
     message("DIRECTX_LIBRARIES = ${DIRECTX_LIBRARIES}")
