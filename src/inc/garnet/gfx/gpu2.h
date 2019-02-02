93e92020d6fee7925910a1adc12ba254dc639250
Chen Li <chenlirandom@gmail.com>
2019-02-01 18:21:43 -0800

    gpu2 header working in progress

--- src/inc/garnet/gfx/gpu2.h
+++ src/inc/garnet/gfx/gpu2.h
@@ -13,15 +13,15 @@ namespace gfx
 
     struct GpuProgramSource
     {
-        ShadingLanguage lang;      ///< shading language
-        bool            debugable; ///< set to true to generate shader binary with debug information.
-        bool            optimized; ///< set to true to generate optimized shader.
-        const char *    vs;
-        const char *    ts;
-        const char *    ds;
-        const char *    gs;
-        const char *    ps;
-        const char *    cs;
+        ShadingLanguage lang; ///< shading language
+        bool            debugable = false; ///< set to true to generate shader binary with debug information.
+        bool            optimized = true;  ///< set to true to generate optimized shader.
+        const char *    vs = nullptr;
+        const char *    ts = nullptr;
+        const char *    ds = nullptr;
+        const char *    gs = nullptr;
+        const char *    ps = nullptr;
+        const char *    cs = nullptr;
     };
 
     struct CommandListCreationParameters
@@ -36,13 +36,27 @@ namespace gfx
 
     struct GpuResourceCreationParameters
     {
-        GpuMemPool * pool;
-        uint64_t     offset;
+        union
+        {
+            struct TextureDesc
+            {
+
+            };
+
+            struct BufferDesc
+            {
+
+            };
+        };
+        GpuMemPool * pool = nullptr;
+        uint64_t     offset = 0; // ignored if pool is null.
     };
 
-    struct RenderStateCreationParameters
+    struct GpuPipelineCreationParameters
     {
-
+        const uint8_t * compiledGpuProgramBinary;
+        uint64_t        compiledGpuProgramSizeInBytes;
+        const char *    states; // render states defined in json format.
     };
 
     struct GpuQueryCreationParameters
@@ -53,22 +67,10 @@ namespace gfx
     class GpuResource : public RefCounter
     {
         /// For immediate update, no dealy, no async. do hazard tracking yourself.
-        virtual void * getDirectPointer(uint32 subResourceId, uint32 * pRowPitch, uint32 * pSlicePitch) = 0;
-    };
-
-    class GpuResourceTable : public RefCounter
-    {
-    };
-
-    class GpuProgram : public RefCounter
-    {
+        virtual void * getDirectPointer(uint32_t subResourceId, uint32_t * pRowPitch, uint32_t * pSlicePitch) = 0;
     };
 
     class GpuPipeline : public RefCounter
-    {
-    };
-
-    class RenderState : public RefCounter
     {
     };
 
@@ -79,17 +81,18 @@ namespace gfx
     struct CopyParameters
     {
         GpuResource * source;
-        uint32        sourceSubresourceId;
-        uint32        destSubresourceId;
+        uint32_t      sourceSubresourceId;
+        GpuResource * dest;
+        uint32_t      destSubresourceId;
     };
 
     struct DrawParameters
     {
         const GpuPipeline * pipeline;
-        uint32              indexcount;
-        uint32              basevertex;
-        uint32              baseindex;
-        uint32              primitive;
+        uint32_t            indexcount;
+        uint32_t            basevertex;
+        uint32_t            baseindex;
+        uint32_t            primitive;
     };
 
     struct PresentParameters
@@ -100,29 +103,27 @@ namespace gfx
     class CommandList : public RefCounter
     {
     public:
-        virtual void   copy(const CopyParameters &) = 0;
-        virtual void   draw(const DrawParameters *) = 0;
-        virtual uint64 mark() = 0; // insert a new fence into command list, returns fence id.
-        virtual void   wait(uint64 fence) = 0; // insert a wait-for-fence into command list
+        virtual void     copy(const CopyParameters &) = 0;
+        virtual void     draw(const DrawParameters *) = 0;
+        virtual uint64_t mark() = 0; // insert a new fence into command list, returns fence id.
+        virtual void     wait(uint64_t fence) = 0; // insert a wait-for-fence into command list
     };
 
-    class Gpu : public RefCounter
+    class Gpu2 : public RefCounter
     {
     public:
-        virtual AutoRef<GpuMemPool>  createMemoryPool(size_t sizeInMB) = 0;
+        virtual AutoRef<GpuMemPool>  createMemoryPool(uint64_t sizeInMB) = 0;
         virtual std::vector<uint8>   compileGpuProgram(const GpuProgramSource &) = 0;
         virtual AutoRef<CommandList> createCommandList(const CommandListCreationParameters &) = 0;
         virtual AutoRef<GpuResource> createResource(const GpuResourceCreationParameters &) = 0;
-        virtual AutoRef<RenderState> createRenderState(const RenderStateCreationParameters &) = 0;
-        virtual AutoRef<GpuPipeline> createPipeline(GpuProgram & program, RenderState & state) = 0;
-        virtual AutoRef<GpuProgram>  createProgram(const void * compiledBlob, size_t sizeInBytes) = 0;
+        virtual AutoRef<GpuPipeline> createPipeline(const GpuPipelineCreationParameters &) = 0;
         virtual AutoRef<GpuQuery>    createQuery(const GpuQueryCreationParameters &) = 0;
         virtual void                 kickoff(CommandList &) = 0; ///< kick off command list.
         virtual void                 present(const PresentParameters &) = 0;
-        virtual void                 wait(uint64 fence) = 0; ///< block caller thread until the fence completes.
-        virtual bool                 testFence(uint64 fence) = 0;
+        virtual void                 wait(uint64_t fence) = 0; ///< block caller thread until the fence completes.
+        virtual bool                 testFence(uint64_t fence) = 0;
     };
 
-    AutoRef<Gpu> createGpu(...);
-} // end of namespace gfx2
+    AutoRef<Gpu2> createGpu2(...);
+} // end of namespace gfx
 } // end of namespace GN
