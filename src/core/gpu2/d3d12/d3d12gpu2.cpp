49df6c9c9693a86f2d8a1f93bec5cc880c8f3ce8
Chen Li <chenlirandom@gmail.com>
2019-05-31 19:53:12 -0700

    d3d12gpu2 working in progress

--- src/core/gpu2/d3d12/d3d12gpu2.cpp
+++ src/core/gpu2/d3d12/d3d12gpu2.cpp
@@ -18,13 +18,40 @@ GN::gfx::D3D12Gpu2::D3D12Gpu2(const CreationParameters & cp)
     ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&factory)));
 
     AutoComPtr<IDXGIAdapter1> hardwareAdapter;
-    //GetHardwareAdapter(factory, &hardwareAdapter);
+    //TODO: GetHardwareAdapter(factory, &hardwareAdapter);
 
     ThrowIfFailed(D3D12CreateDevice(
         hardwareAdapter,
         D3D_FEATURE_LEVEL_12_0,
         IID_PPV_ARGS(&_device)
         ));
+
+    // create command queue
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    ThrowIfFailed(_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&_commandQueue)));
+
+    // get current window size
+    auto windowSize = cp.window->getClientSize();
+
+    // create swap chain.
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+    swapChainDesc.BufferCount = BACK_BUFFER_COUNT;
+    swapChainDesc.BufferDesc.Width = 0 == cp.width ? windowSize.x : cp.width;
+    swapChainDesc.BufferDesc.Height = cp.height ? windowSize.y : cp.height;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.OutputWindow = (HWND)cp.window->getWindowHandle();
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.Windowed = TRUE;
+    AutoComPtr<IDXGISwapChain> swapChain;
+    ThrowIfFailed(factory->CreateSwapChain(
+        _commandQueue, // Swap chain needs the queue so that it can force a flush on it.
+        &swapChainDesc,
+        &swapChain));
+    ThrowIfFailed(swapChain.as(&_swapChain));
 }
 
 //
