3238e747c105ed3995a96c0a60d6ee17e937b684
randomgraphics <gfxrgb@gmail.com>
2023-12-19 23:40:58 -0800

    cleanup

--- src/inc/garnet/gfx/image.h
+++ src/inc/garnet/gfx/image.h
@@ -4,6 +4,9 @@
 
 #if !GN_BUILD_IS_STATIC
 #define RAPID_IMAGE_SHARED_LIB 1
+#ifdef GN_CORE_INTERNAL
+#define RAPID_IMAGE_EXPORTS 1
+#endif
 #endif
 #define RAPID_IMAGE_NAMESPACE GN::gfx
 #include <rapid-image/rapid-image.h>
