79c5cd03e13ba22cb2636e2fcd308e39ade99fe1
randomgraphics <gfxrgb@gmail.com>
2024-01-24 14:43:14 -0800

    update docker entrypoint script

--- env/docker/ubuntu/entrypoint.sh
+++ env/docker/ubuntu/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 dir="$(cd $(dirname "${BASH_SOURCE[0]}");pwd)"
 cd ${dir}/../../..
-export ANDROID_SDK_ROOT=/android/sdk
-export ANDROID_NDK_ROOT=/android/sdk/ndk/25.2.9519653
-export JAVA_HOME=/android/studio/jbr
 ./setupenv.sh $@
