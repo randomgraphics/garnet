868736165c7adf782b7b5156964fa6050f68b667
Chen Li <chen.li@innopeaktech.com>
2026-02-25 18:39:10 -0800

    wip

--- src/inc/garnet/rdg/pipeline.h
+++ src/inc/garnet/rdg/pipeline.h
@@ -2,49 +2,61 @@
 
 namespace GN::rdg {
 
-struct ShadowMapping {
-    struct CreateParameters {
-        AutoRef<GpuContext> gpu;
+struct PbrShading {
+    /// PbrMaterial represents a PBR material resource.
+    /// Backend is free to determine the best way to represent the material. Texture set, material parameter values, etc.
+    struct Material : public GpuResource {
+        GN_API static const uint64_t         TYPE_ID;
+        inline static constexpr const char * TYPE_NAME = "PbrTextureSet";
+
+        struct CreateParameters {
+            AutoRef<GpuContext> gpu;
+        };
+
+        struct LoadParameters {
+            AutoRef<GpuContext> gpu;
+            StrA                fileName;
+        };
+
+        static GN_API AutoRef<Material> load(ArtifactDatabase & db, const StrA & name, const CreateParameters & params);
+
+    protected:
+        using GpuResource::GpuResource;
     };
 
-    virtual ~ShadowMapping() = default;
+    /// PbrEnvironment represents a PBR environment resource.
+    /// Bacnend is free to determine the best way to represent the environment. Cubemap, latlong map, along with
+    /// any other auxiliary data (like exposure, tone mapping, etc.) to define this environment.
+    struct Environment : public GpuResource {
+        GN_API static const uint64_t         TYPE_ID;
+        inline static constexpr const char * TYPE_NAME = "PbrEnvironment";
 
-    static GN_API ShadowMapping * create(ArtifactDatabase & db, const StrA & name, const CreateParameters & params);
+        struct CreateParameters {
+            AutoRef<GpuContext> gpu;
+        };
 
-protected:
-    ShadowMapping() = default;
-};
+        struct LoadParameters {
+            AutoRef<GpuContext> gpu;
+            StrA                fileName;
+        };
 
-struct PbrShading {
+        /// Create a new blank (full black) environment resource.
+        static GN_API AutoRef<Environment> create(ArtifactDatabase & db, const StrA & name, const CreateParameters & params);
+
+        /// Load a Pbr environment resource from external file.
+        static GN_API AutoRef<Environment> load(ArtifactDatabase & db, const StrA & name, const CreateParameters & params);
+
+    };
+
+
     struct Inputs {
-        AutoRef<GpuContext> gpu;
+        AutoRef<Material>    material;
+        AutoRef<Environment> environment;
 
-        /// Albedo map.
-        TextureView albedo;
-
-        /// Normal map.
-        TextureView normal;
-
-        /// Roughness-ambient-metallic map.
-        /// Red channel: roughness
-        /// Green channel: metallic
-        /// Blue channel: optional ambient occlusion.
-        TextureView ram;
-
-        /// Separated occlusion map. Use a separate occlusion map because it is often in much lower resolution than the other maps.
-        TextureView ao;
-
-        /// Environment map.
-        TextureView environment;
-
-        /// Model-to-world matrix.
-        Matrix44f modelMatrix;
-
-        /// World-to-view matrix.
-        Matrix44f viewMatrix;
-
-        /// View-to-clip matrix.
-        Matrix44f projMatrix;
+        /// \todo find a way to encapsulate these matrices into another concetp to allow
+        /// each backend to customize it properly.
+        /// For now, just use the standard matrix for simplicity.
+        Matrix44f modelToWorld, worldToView, viewToClip;
     };
 
     struct Outputs {
