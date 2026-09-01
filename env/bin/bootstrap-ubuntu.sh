2a4fe8722011790ac67c5db6fbc95be60cbcf4a8
randomgraphics <gfxrgb@gmail.com>
2026-09-01 04:49:54 +0000

    fix linux setup script on ubuntu 26

--- env/bin/bootstrap-ubuntu.sh
+++ env/bin/bootstrap-ubuntu.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/bash
+set -eo pipefail
 
-echo "This script installs garnet build dependencies to the current host Ubuntu 22.04. To stop, press Ctrl-C now. Or press ENTER to continue..."
+echo "This script installs garnet build dependencies to the current host Ubuntu 22.04+. To stop, press Ctrl-C now. Or press ENTER to continue..."
 read
-
-# Setup Vulkan SDK repo
-wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
-sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.3.275-jammy.list https://packages.lunarg.com/vulkan/1.3.275/lunarg-vulkan-1.3.275-jammy.list
 
 # install build dependencies 
 sudo apt-get update
 sudo apt-get install -y \
     build-essential \
+    clang \
     cmake \
     git \
     git-lfs \
     ninja-build \
-    vulkan-sdk \
-    clang-15 \
-    clang-format-14 \
-    libxkbcommon-dev \
+    python3-dev \
+    python3-pip \
     pkg-config \
-
-# install python modules
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade termcolor
+    libxkbcommon-dev \
+    libxinerama-dev \
+    libxcursor-dev \
+    libxi-dev \
